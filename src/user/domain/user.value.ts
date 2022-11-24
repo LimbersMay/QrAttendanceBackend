8b2588805543c34871bca59d5a2c5e5fc7184463
limbersMay <limbermay1@gmail.com>
2022-11-24 14:07:17 -0600

    se corrigió el nombre de los atributos

--- src/user/domain/user.value.ts
+++ src/user/domain/user.value.ts
@@ -9,12 +9,12 @@ export class UserValue implements UserEntity {
     mothersName: string;
     fathersName: string;
 
-    constructor ({email, password, name, mothersName: mothers_name, fathersName: fathers_name}: UserEntity) {
+    constructor ({name, email, password, mothersName, fathersName}: UserEntity) {
         this.email = email;
         this.password = password;
         this.name = name;
-        this.mothersName = mothers_name;
-        this.fathersName = fathers_name;
+        this.mothersName = mothersName;
+        this.fathersName = fathersName;
 
         this.userId = uuid();
     }
