ff90be8f8ca9ffadfa636f4500c3d0a2cb3b1755
limbersMay <limbermay1@gmail.com>
2022-11-25 17:09:56 -0600

    se agregó la dependencia del mapper

--- src/user/infraestructure/repository/mysql.repository.ts
+++ src/user/infraestructure/repository/mysql.repository.ts
@@ -1,12 +1,16 @@
-import { UserEntity } from '../../domain/user.entity';
-import { UserRepository } from '../../domain/user.repository';
+import {UserEntity} from '../../domain/user.entity';
+import {UserRepository} from '../../domain/user.repository';
+
 import User from '../model/user.schema';
+import {UserMapperService} from "../mappers/user.mapper";
 
 export class MysqlRepository implements UserRepository {
 
-    async findUserById(userId: string): Promise<any> {
+    public constructor(private userMapperService: UserMapperService){}
+
+    async findUserById(userId: string): Promise<UserEntity | null> {
         const user = await User.findByPk(userId);
-        return user;
+        return this.userMapperService.toDomain(user);
     }
 
     async loginUser({email, password}: {email: string, password: string}): Promise<UserEntity | null> {
