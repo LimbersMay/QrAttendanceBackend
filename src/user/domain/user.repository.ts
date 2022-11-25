ac12a772df95ca48dc94b3d02fe497dd76ada9ca
limbersMay <limbermay1@gmail.com>
2022-11-25 16:41:12 -0600

    se renombró el método de login a loginUser

--- src/user/domain/user.repository.ts
+++ src/user/domain/user.repository.ts
@@ -3,6 +3,6 @@ import { UserEntity } from "./user.entity";
 export interface UserRepository{
     findUserById(userId: string): Promise<UserEntity | null>;
     createUser(user: UserEntity): Promise<UserEntity | null>;
-    login({email, password}: {email: string, password: string}): Promise<UserEntity | null>;
+    loginUser({email, password}: {email: string, password: string}): Promise<UserEntity | null>;
     listUser(): Promise<UserEntity | null>;
 }
