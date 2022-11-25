7fba45be9b2e9d2639a887afec261f7c5e5160f9
limbersMay <limbermay1@gmail.com>
2022-11-25 16:17:14 -0600

    se agregó la función para iniciar sesión en el repositorio

--- src/user/domain/user.repository.ts
+++ src/user/domain/user.repository.ts
@@ -2,6 +2,7 @@ import { UserEntity } from "./user.entity";
 
 export interface UserRepository{
     findUserById(userId: string): Promise<UserEntity | null>;
-    registerUser(user: UserEntity): Promise<UserEntity | null>;
+    createUser(user: UserEntity): Promise<UserEntity | null>;
+    login({email, password}: {email: string, password: string}): Promise<UserEntity | null>;
     listUser(): Promise<UserEntity | null>;
 }
