757bd481c0703105e7163b8b4f0c9956f363e4f5
limbersMay <limbermay1@gmail.com>
2022-11-25 23:24:01 -0600

    se agregó la opción para crear usuarios en el repositorio

--- src/user/infraestructure/repository/mysql.repository.ts
+++ src/user/infraestructure/repository/mysql.repository.ts
@@ -2,15 +2,15 @@ import {UserEntity} from '../../domain/user.entity';
 import {UserRepository} from '../../domain/user.repository';
 
 import User from '../model/user.schema';
-import {UserMapperService} from "../mappers/user.mapper";
+import {DtoMapperService, UserMapperService} from "../mappers/user.mapper";
 
 export class MysqlRepository implements UserRepository {
 
-    public constructor(private userMapperService: UserMapperService){}
+    public constructor(private userMapperService: UserMapperService, private dtoMapperService: DtoMapperService){}
 
     async findUserById(userId: string): Promise<UserEntity | null> {
         const user = await User.findByPk(userId);
-        return this.userMapperService.toDomain(user);
+        return this.userMapperService.transform(user);
     }
 
     async loginUser({email, password}: {email: string, password: string}): Promise<UserEntity | null> {
@@ -18,7 +18,13 @@ export class MysqlRepository implements UserRepository {
     }
 
     async createUser(user: UserEntity): Promise<UserEntity | null> {
-        throw new Error('Method not implemented.');
+        const mappedUser = this.dtoMapperService.transform(user);
+
+        mappedUser.modifiedAt = new Date();
+        mappedUser.createdAt = new Date();
+
+        const userCreated = await User.create(mappedUser);
+        return this.userMapperService.transform(userCreated);
     }
 
     async listUser(): Promise<UserEntity | null> {
