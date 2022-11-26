23e7dffb5b4d84c29a6471c9263cefe3d61d67e9
limbersMay <limbermay1@gmail.com>
2022-11-25 23:20:57 -0600

    se cambió de nombre de toDomain a transform

--- src/user/infraestructure/mappers/mapper.ts
+++ src/user/infraestructure/mappers/mapper.ts
@@ -1,16 +1,16 @@
 interface Mapper<S, T> {
-    toDomain(entity: S): T;
-    toDomain(array: S[]): T[];
-    toDomain(entityOrArray: S | S[]): T | T[];
+    transform(entity: S): T;
+    transform(array: S[]): T[];
+    transform(entityOrArray: S | S[]): T | T[];
 }
 
 export abstract class MapperService<S, T> implements Mapper<S, T> {
 
     protected abstract map(entity: S): T;
 
-    toDomain(entity: S): T;
-    toDomain(array: S[]): T[];
-    toDomain(entityOrArray: S | S[]): T | T[] {
+    transform(entity: S): T;
+    transform(array: S[]): T[];
+    transform(entityOrArray: S | S[]): T | T[] {
 
         if (Array.isArray(entityOrArray)) {
             return entityOrArray.map((item: S) => this.map(item));
