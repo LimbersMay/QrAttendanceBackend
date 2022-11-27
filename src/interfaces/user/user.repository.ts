7ee4526bdae60f79f55c3fa8669f999e05169a9b
limbersMay <limbermay1@gmail.com>
2022-11-27 15:46:34 -0600

    Se agregó la función para buscar un usuario por email

--- src/interfaces/user/user.repository.ts
+++ src/interfaces/user/user.repository.ts
@@ -3,6 +3,7 @@ import { UserEntity } from "./user.entity";
 
 export interface UserRepository{
     findUserById(userId: string): Promise<UserEntity | null>;
+    findUserByEmail(userEmail: string): Promise<UserEntity | null>;
     createUser(user: UserEntity): Promise<UserEntity | null>;
     modifyUser(user: UserEntity): Promise<UserEntity | null>;
     listUser(): Promise<UserEntity | null>;
