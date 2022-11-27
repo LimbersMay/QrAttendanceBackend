4a793a2591f6a0d7ed362904116930ea8aad50b3
limbersMay <limbermay1@gmail.com>
2022-11-27 14:10:00 -0600

    se creó la clase para mapear entidades del usuario

---
+++ src/mappers/user.mapper.ts
@@ -0,0 +1,29 @@
+import {MapperService} from "./mapper";
+import {UserEntity} from "../interfaces/user/user.entity";
+
+export class UserMapperService extends MapperService<any, UserEntity> {
+    protected mapToDomain(entity: any): UserEntity {
+        return {
+            name: entity.name,
+            email: entity.email,
+            password: entity.password,
+            mothersName: entity.mothers_name,
+            fathersName: entity.fathers_name,
+            updatedAt: entity.updated_at,
+            createdAt: entity.created_at
+        }
+    }
+
+    protected mapToDto(entity: UserEntity): any {
+        return {
+            user_id: entity.userId,
+            name: entity.name,
+            email: entity.email,
+            password: entity.password,
+            mothers_name: entity.mothersName,
+            fathers_name: entity.fathersName,
+            updated_at: entity.updatedAt,
+            created_at: entity.createdAt
+        }
+    }
+}
