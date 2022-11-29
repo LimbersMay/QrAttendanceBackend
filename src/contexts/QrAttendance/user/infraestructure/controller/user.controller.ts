dcd56ec598003233d1d2b0645a8c346df99f8987
limbersMay <limbermay1@gmail.com>
2022-11-28 22:35:19 -0600

    se agregaron y usaron las dependencias del userUseCase, encryptService y uuidService

--- src/contexts/QrAttendance/user/infraestructure/controller/user.controller.ts
+++ src/contexts/QrAttendance/user/infraestructure/controller/user.controller.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
 import { UserUseCase } from '../../application/userUseCase';
 
+import {EncryptService} from "../../../shared/infraestructure/adapters/encrypt.service";
+import {UuiService} from "../../../shared/infraestructure/adapters/uui.service";
+
 export class UserController {
-    constructor(private userUseCase: UserUseCase) {}
+    constructor(
+        private userUseCase: UserUseCase,
+        private encryptService: EncryptService,
+        private uuidService: UuiService
+    ) {}
 
     public getCtrl = async({ query }: Request, res: Response) => {
 
@@ -15,10 +22,14 @@ export class UserController {
     }
 
     public insertCtrl = async({ body }: Request, res: Response ) => {
-        const { name, email,  password, mothersName, fathersName } = body;
+        let { name, email,  password, mothersName, fathersName } = body;
 
-        const user = await this.userUseCase.registerUser({ name, email, password, mothersName, fathersName });
-        return res.status(200).json({
+        const userId = this.uuidService.random();
+        password = await this.encryptService.hash(password);
+
+        const user = await this.userUseCase.registerUser({ userId, name, email, password, mothersName, fathersName });
+        res.status(200);
+        res.json({
             user
         });
     }
