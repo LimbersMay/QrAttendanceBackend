efc327b1bac4635916d44ce18d9743e807704067
limbersMay <limbermay1@gmail.com>
2022-11-28 22:59:05 -0600

    se agregaron los adapdores para encryptar/hash y para generar un código uuid

--- src/contexts/QrAttendance/user/infraestructure/routes/user.route.ts
+++ src/contexts/QrAttendance/user/infraestructure/routes/user.route.ts
@@ -1,17 +1,25 @@
 
 import { Router } from "express";
-import { MysqlRepository } from '../repository/mysql.repository';
+import { UserMysqlRepository } from '../repository/userMysqlRepository';
 import { UserUseCase } from '../../application/userUseCase';
 import { UserController } from '../controller/user.controller';
 import { UserMapperService } from '../mappers/user.mapper';
+import {BcryptAdapter} from "../adapters/bcryptAdapter";
+import {UuidAdapter} from "../adapters/uuid.adapter";
 
 const userRouter = Router();
+
+/**
+ * Iniciamos los adaptadores
+ */
+const bycriptAdapter = new BcryptAdapter();
+const uuidAdapter = new UuidAdapter();
 
 /**
  * Iniciamos el repositorio
  */
 
-const mysqlRepository = new MysqlRepository(new UserMapperService());
+const mysqlRepository = new UserMysqlRepository(new UserMapperService());
 
 /**
  * Iniciamos casos de uso 
@@ -21,12 +29,12 @@ const userUseCase = new UserUseCase(mysqlRepository);
 /**
  * Iniciamos el User controllers
  */
-const userCtrl = new UserController(userUseCase);
+const userCtrl = new UserController(userUseCase, bycriptAdapter, uuidAdapter);
 
 /**
  * 
  */
 userRouter.get('/user', userCtrl.getCtrl);
-userRouter.post('/user/signin', userCtrl.insertCtrl);
+userRouter.post('/api/user/signin', userCtrl.insertCtrl);
 
 export default userRouter;
