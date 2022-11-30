075f9b3fdd40413ff08c49127cc9e4af116a1ccc
limbersMay <limbermay1@gmail.com>
2022-11-30 15:09:36 -0600

    se cambió el nombre de la función editRegistry(updateRegistry)

--- src/contexts/QrAttendance/registry/domain/registry.repository.ts
+++ src/contexts/QrAttendance/registry/domain/registry.repository.ts
@@ -3,6 +3,6 @@ import { RegistryEntity } from './registry.entity';
 export interface IRegistryRepository {
     createRegistry(registry: RegistryEntity): Promise<RegistryEntity | null>;
     deleteRegistry(registryId: string): Promise<RegistryEntity | null>;
-    editRegistry(registry: RegistryEntity, registryId: string): Promise<RegistryEntity | null>;
+    updateRegistry(registry: RegistryEntity, registryId: string): Promise<RegistryEntity | null>;
     findRegistryById(registryId: string): Promise<RegistryEntity | null>;
 }
