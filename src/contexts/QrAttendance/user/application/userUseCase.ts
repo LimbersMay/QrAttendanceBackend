6d1b7f2d7fa8cdf8427f94a7804c131b1721473f
limbersMay <limbermay1@gmail.com>
2022-11-30 08:54:25 -0600

    se recibió el ID del usuario al registrarlo

--- src/contexts/QrAttendance/user/application/userUseCase.ts
+++ src/contexts/QrAttendance/user/application/userUseCase.ts
@@ -1,24 +1,17 @@
-import { UserRepository } from '../domain/user.repository';
-import { UserEntity } from '../domain/user.entity';
-import { UserValue } from '../domain/user.value';
+import {UserRepository} from '../domain/user.repository';
+import {UserEntity} from '../domain/user.entity';
+import {UserValue} from '../domain/user.value';
 
 export class UserUseCase {
     constructor( private readonly userRepository: UserRepository ) {}
 
-    public registerUser = async ({name, email, password, mothersName, fathersName}: UserEntity) => {
-        const userValue = new UserValue({name, email, password, mothersName, fathersName});
-        const userCreated = await this.userRepository.createUser(userValue)
-        return userCreated;
-    }
-
-    public loginUser = async ({email, password}: {email: string, password: string}) => {
-        const user = await this.userRepository.loginUser({email, password});
-        return user;
+    public registerUser = async ({userId, name, email, password, mothersName, fathersName}: UserEntity) => {
+        const userValue = new UserValue({userId, name, email, password, mothersName, fathersName});
+        return await this.userRepository.createUser(userValue);
     }
 
     public findUserById = async (userId: string) => {
-        const user = await this.userRepository.findUserById(userId);
-        return user;
+        return await this.userRepository.findUserById(userId);
     }
 
     public registerQr = async () => {};
