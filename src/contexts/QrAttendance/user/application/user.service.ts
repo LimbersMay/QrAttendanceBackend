1b93706d9ab18cc391150fe615a0003c097d91a7
limbersMay <limbermay1@gmail.com>
2022-11-30 09:10:23 -0600

    se creó el userService para manejar la lógica del usuario

--- src/contexts/QrAttendance/user/application/user.service.ts
+++ src/contexts/QrAttendance/user/application/user.service.ts
@@ -1,11 +1,21 @@
 import {UserRepository} from '../domain/user.repository';
 import {UserEntity} from '../domain/user.entity';
 import {UserValue} from '../domain/user.value';
+import {EncryptService} from "../../shared/application/services/encrypt.service";
+import {UuiService} from "../../shared/application/services/uui.service";
 
-export class UserUseCase {
-    constructor( private readonly userRepository: UserRepository ) {}
+export class UserService {
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly encryptService: EncryptService,
+        private readonly uuidService: UuiService
+    ) {}
 
     public registerUser = async ({userId, name, email, password, mothersName, fathersName}: UserEntity) => {
+
+        userId = this.uuidService.random();
+        password = await this.encryptService.hash(password);
+
         const userValue = new UserValue({userId, name, email, password, mothersName, fathersName});
         return await this.userRepository.createUser(userValue);
     }
