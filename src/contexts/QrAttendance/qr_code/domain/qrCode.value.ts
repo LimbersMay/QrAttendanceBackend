f4001d18f66f50bdf70f9529dd31fb3a132c4278
limbersMay <limbermay1@gmail.com>
2022-12-08 21:39:30 -0600

    (qr_code) Atributo name agregado al value object

--- src/contexts/QrAttendance/qr_code/domain/qrCode.value.ts
+++ src/contexts/QrAttendance/qr_code/domain/qrCode.value.ts
@@ -3,14 +3,15 @@ import {QrCodeEntity} from "./qrCode.entity";
 export class QrCodeValue implements QrCodeEntity {
     qrId: string;
     userId: string;
+    name: string;
     url: string;
     createdAt: Date | undefined;
     updatedAt: Date | undefined;
 
-    constructor({qrId, userId, url, createdAt, updatedAt}: QrCodeEntity) {
+    constructor({qrId, userId, name, url, createdAt, updatedAt}: QrCodeEntity) {
         this.qrId = qrId;
         this.userId = userId;
-
+        this.name = name;
         this.url = url;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
