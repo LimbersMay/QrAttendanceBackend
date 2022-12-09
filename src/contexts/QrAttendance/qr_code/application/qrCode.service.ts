5301d61634706f1c4235bdda0abaa05603a157aa
limbersMay <limbermay1@gmail.com>
2022-12-08 21:47:50 -0600

    (qr_code) Plantilla del QrCode service agregado

---
+++ src/contexts/QrAttendance/qr_code/application/qrCode.service.ts
@@ -0,0 +1,21 @@
+import {QrCodeRepository} from "../domain/qrCode.repository";
+import {QrCodeEntity} from "../domain/qrCode.entity";
+
+export class QrCodeService implements QrCodeRepository {
+    createQrCode(qrCode: QrCodeEntity): Promise<QrCodeEntity | null> {
+        throw new Error("Not implemented");
+    }
+
+    deleteQrCode(qrCodeId: string): Promise<QrCodeEntity | null> {
+        throw new Error("Not implemented");
+    }
+
+    findQrCodeById(qrCodeId: string): Promise<QrCodeEntity | null> {
+        throw new Error("Not implemented");
+    }
+
+    updateQrCode(fields: any, qrCodeId: string): Promise<QrCodeEntity | null> {
+        throw new Error("Not implemented");
+    }
+
+}
