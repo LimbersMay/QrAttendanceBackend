3301eca50b71d465ed1749dc8c36b501da1376e5
limbersMay <limbermay1@gmail.com>
2022-12-09 20:09:56 -0600

    (main) Se cambió el archivo de app.ts

--- src/server.app.ts
+++ src/server.app.ts
@@ -1,7 +1,19 @@
+import "dotenv/config";
+import express from 'express';
+import cors from "express";
+import userRouter from "./contexts/QrAttendance/user/infraestructure/routes/user.route";
+import db from "./contexts/shared/infraestructure/db/mysql.connection";
 
-export class ServerApp {
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-    constructor() {
-
-    }
-}
+const port = process.env.PORT || 3001;
+app.use(userRouter);
+db.authenticate().then();
+db.sync().then(result => {
+    app.listen(port, () => console.log(`USER LISTO POR EL PUERTO ${port}`));
+})
+    .catch(error => {
+        console.log(error);
+    });
