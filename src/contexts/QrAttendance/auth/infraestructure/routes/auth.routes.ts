67c5679bdef4b6f31fa5aad96c7679a8ddab3571
limberMay <limbermay1@gmail.com>
2023-01-23 21:34:54 -0600

    (qrAttendance/auth) archivo de authRoutes creado para el manejo de las rutas de la authenticación

---
+++ src/contexts/QrAttendance/auth/infraestructure/routes/auth.routes.ts
@@ -0,0 +1,16 @@
+import { Router} from "express";
+import {UserMapperService} from "../../../user/infraestructure/mappers/user.mapper";
+import {AuthController} from "../controller/auth.controller";
+
+const router = Router();
+
+const userMapper = new UserMapperService();
+const authController = new AuthController(userMapper);
+
+router.post("/login-local", authController.loginLocal);
+
+router.post("/logout", authController.logout);
+
+router.get("/authenticated", authController.isAuthenticated);
+
+export default router;
