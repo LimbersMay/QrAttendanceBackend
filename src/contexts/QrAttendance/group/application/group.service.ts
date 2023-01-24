b070bf52bf97cef6e7b3be974726b51d4aebd1ea
limberMay <limbermay1@gmail.com>
2023-01-24 15:43:22 -0600

    (contexts/qrAttendance/group) Crud parcialmente implementado con los metodos de Create, Delete y findByUserId

---
+++ src/contexts/QrAttendance/group/application/group.service.ts
@@ -0,0 +1,62 @@
+import {GroupRepository} from "../infraestructure/repository";
+import {UUIDGenerator} from "../../shared/application/services/UUIDGenerator";
+import {Either} from "../../../shared/types/ErrorEither";
+import {GroupEntity} from "../domain/group.entity";
+import {GroupError} from "./errors/group.errors";
+import {GroupValue} from "../domain/group.value";
+import {left, right} from "fp-ts/Either";
+import {GroupMapper} from "../infraestructure/mappers/group.mapper";
+import {DateFormatter} from "../../shared/application/services/dateFormatter";
+
+export class GroupService {
+
+    constructor(
+        private readonly groupRepository: GroupRepository,
+        private readonly uuidGenerator: UUIDGenerator,
+        private readonly groupMapper: GroupMapper,
+        private readonly dateFormatter: DateFormatter
+    ){
+    }
+
+    async createGroup(name: string, userId: string, timezone: string): Promise<Either<GroupError, GroupEntity>> {
+
+        const group = GroupValue.create({
+            groupId: this.uuidGenerator.random(),
+            userId,
+            name,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        });
+
+        const groupPersistance = this.groupMapper.toPersistance(group);
+
+        await this.groupRepository.createGroup(groupPersistance);
+        if (!group) return left(GroupError.GROUP_NOT_CREATED);
+
+        const mapped = this.groupMapper.toDTO(group);
+
+        return right(mapped);
+    }
+
+    async getGroupsByUserId(userId: string): Promise<Either<GroupError, GroupEntity[]>> {
+
+        const groups = await this.groupRepository.findGroupsByUserId(userId);
+        if (!groups) return left(GroupError.CANNOT_GET_GROUPS);
+
+        const groupsDomain = this.groupMapper.toDomain(groups);
+        const groupsDto = this.groupMapper.toDTO(groupsDomain);
+
+        return right(groupsDto);
+    }
+
+    async deleteGroup(groupId: string): Promise<Either<GroupError, GroupEntity>> {
+        const group = await this.groupRepository.findGroupById(groupId);
+        if (!group) return left(GroupError.GROUP_NOT_FOUND);
+
+        await this.groupRepository.deleteGroup(groupId);
+
+        const mapped = this.groupMapper.toDomain(group);
+
+        return right(this.groupMapper.toDTO(mapped));
+    }
+}
