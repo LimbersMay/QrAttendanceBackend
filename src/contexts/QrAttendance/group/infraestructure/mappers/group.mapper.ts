9102e00d2d67b293f557b4bed3b589d78d7bf80f
limberMay <limbermay1@gmail.com>
2023-01-24 13:11:28 -0600

    (contexts/qrAttendance/group) data mapper de group creado

---
+++ src/contexts/QrAttendance/group/infraestructure/mappers/group.mapper.ts
@@ -0,0 +1,36 @@
+import {MapperService} from "../../../shared/infraestructure/mappers/mapper";
+import {GroupEntity} from "../../domain/group.entity";
+import {GroupDTO} from "../../application/entities/group.dto";
+import {GroupPersistance} from "../../application/entities/group.persistance";
+
+export class GroupMapper extends MapperService<any, GroupEntity> {
+    protected mapToDTO(entity: GroupEntity): GroupDTO {
+        return {
+            id: entity.groupId,
+            name: entity.name,
+            createdAt: entity.createdAt,
+            updatedAt: entity.updatedAt
+        }
+    }
+
+    // from persistance to domain
+    protected mapToDomain(entity: GroupPersistance): GroupEntity {
+        return {
+            groupId: entity.group_id,
+            userId: entity.user_id,
+            name: entity.name,
+            createdAt: entity.created_at,
+            updatedAt: entity.updated_at
+        };
+    }
+
+    protected mapToPersistance(entity: GroupEntity): GroupPersistance {
+        return {
+            group_id: entity.groupId,
+            user_id: entity.userId,
+            name: entity.name,
+            created_at: entity.createdAt,
+            updated_at: entity.updatedAt
+        };
+    }
+}
