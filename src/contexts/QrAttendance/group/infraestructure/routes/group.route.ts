7e569e2e5261101083623c1cd04321b3997b2c60
limberMay <limbermay1@gmail.com>
2023-01-24 16:18:36 -0600

    (contexts/qrAttendance/group) route de la entidad creada con un CRUD básico (post, delete, get)

---
+++ src/contexts/QrAttendance/group/infraestructure/routes/group.route.ts
@@ -0,0 +1,20 @@
+import {Router} from "express";
+import {GroupRepository} from "../repository";
+import {GroupService} from "../../application/group.service";
+import {UuidAdapter} from "../adapters";
+import {GroupMapper} from "../mappers";
+import {GroupController} from "../controller/group.controller";
+
+const router = Router();
+
+const groupRepository = new GroupRepository();
+const uuidGenerator = new UuidAdapter();
+const groupMapper = new GroupMapper();
+
+const groupService = new GroupService(groupRepository, uuidGenerator, groupMapper);
+
+const groupController = new GroupController(groupService);
+
+router.post('/create', groupController.createGroup);
+router.get('/findAll/:userId', groupController.findGroupByUserId);
+router.delete('/delete', groupController.deleteGroup);
