edef31274b81e52af529db40312d19975d2d8d06
limberMay <limbermay1@gmail.com>
2023-01-24 16:18:57 -0600

    (contexts/qrAttendance/group) controller de la entidad Group creada con los métodos del router

---
+++ src/contexts/QrAttendance/group/infraestructure/controller/group.controller.ts
@@ -0,0 +1,65 @@
+import {GroupService} from "../../application/group.service";
+import {Request, Response} from "express";
+import {isLeft} from "fp-ts/Either";
+
+export class GroupController {
+    constructor(
+        private readonly groupService: GroupService
+    ) {
+    }
+
+    createGroup = async (req: Request, res: Response) => {
+        const {name, userId} = req.body;
+
+        const group = await this.groupService.createGroup(name, userId);
+
+        if (isLeft(group)) {
+            return res.status(400).json({
+                ok: false,
+                msg: group.left
+            })
+        }
+
+        res.status(201).json({
+            ok: true,
+            group: group.right
+        });
+    }
+
+    findGroupByUserId  = async(req: Request, res: Response) => {
+        const { userId } = req.params;
+
+        const groups = await this.groupService.getGroupsByUserId(userId);
+
+        if (isLeft(groups)) {
+            return res.status(400).json({
+                ok: false,
+                msg: groups.left
+            });
+        }
+
+        res.status(400).json({
+            ok: false,
+            groups: groups.right
+        });
+    }
+
+    deleteGroup = async(req: Request, res: Response) => {
+
+        const { id } = req.body;
+
+        const group = await this.groupService.deleteGroup(id);
+
+        if (isLeft(group)) {
+            return res.status(400).json({
+                ok: false,
+                msg: group.left
+            });
+        }
+
+        res.status(400).json({
+            ok: false,
+            group: group.right
+        });
+    }
+}
