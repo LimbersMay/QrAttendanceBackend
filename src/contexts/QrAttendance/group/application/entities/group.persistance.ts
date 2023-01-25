c7e1e14a515293dea55ff87614938d12518098d9
limberMay <limbermay1@gmail.com>
2023-01-24 19:09:26 -0600

    (qrAttendance/group) se cambiaron los atributos de created_at y updated_at en la interfaz de GroupPersistance

--- src/contexts/QrAttendance/group/application/entities/group.persistance.ts
+++ src/contexts/QrAttendance/group/application/entities/group.persistance.ts
@@ -3,6 +3,6 @@ export interface GroupPersistance {
     group_id: string,
     user_id: string,
     name: string,
-    created_at: Date,
-    updated_at: Date
+    createdAt: Date,
+    updatedAt: Date
 }
