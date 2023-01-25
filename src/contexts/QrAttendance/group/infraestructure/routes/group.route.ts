a4c505aefd80f2d0ca6032289c75c903724008d1
limberMay <limbermay1@gmail.com>
2023-01-25 15:33:23 -0600

    (qrAttendance/group) middlewares para autenticar la sesión y el grupo a eliminar creado en la ruta de grupos

--- src/contexts/QrAttendance/group/infraestructure/routes/group.route.ts
+++ src/contexts/QrAttendance/group/infraestructure/routes/group.route.ts
@@ -4,6 +4,8 @@ import {GroupService} from "../../application/group.service";
 import {UuidAdapter} from "../adapters";
 import {GroupMapper} from "../mappers";
 import {GroupController} from "../controller/group.controller";
+import {isAuthenticated} from "../../../auth/infraestructure/middlewares";
+import {GroupMiddleware} from "../middlewares/group.middleware";
 
 const router = Router();
 
@@ -14,10 +16,20 @@ const groupMapper = new GroupMapper();
 const groupService = new GroupService(groupRepository, uuidGenerator, groupMapper);
 
 const groupController = new GroupController(groupService);
+const groupMiddleware = new GroupMiddleware(groupService);
 
-router.post('/create', groupController.createGroup);
-router.get('/all', groupController.getUserGroups);
-router.put('/update', groupController.updateGroup);
-router.delete('/delete', groupController.deleteGroup);
+router.post('/create', isAuthenticated, groupController.createGroup);
+
+router.get('/all', isAuthenticated, groupController.getUserGroups);
+
+router.put('/update', [
+    isAuthenticated,
+    groupMiddleware.validateGroupExists
+], groupController.updateGroup);
+
+router.delete('/delete', [
+    isAuthenticated,
+    groupMiddleware.validateGroupExists
+], groupController.deleteGroup);
 
 export default router;
