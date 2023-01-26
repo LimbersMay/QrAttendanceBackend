c082ae551bd01d71b4bedc115f58a979c171ece1
limberMay <limbermay1@gmail.com>
2023-01-26 14:01:28 -0600

    (qrAttendance/user) Modelo de User re definido usando sequelize-typescript con decoradores

--- src/contexts/QrAttendance/user/infrastructure/model/user.schema.ts
+++ src/contexts/QrAttendance/user/infrastructure/model/user.schema.ts
@@ -1,49 +1,26 @@
-import {DataTypes} from "sequelize";
-import db from '../../../../shared/infrastructure/db/mysql.connection';
-import Group from "../../../group/infrastructure/model/group.schema";
+import {Column, Model, PrimaryKey, Table, Unique} from "sequelize-typescript";
+import {UserEntity} from "../../domain/user.entity";
 
-const User = db.define('user', {
-        user_id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING
-        },
-        email: {
-            type: DataTypes.STRING
-        },
-        password: {
-            type: DataTypes.STRING
-        },
-        lastname: {
-            type: DataTypes.STRING
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            field: 'created_at'
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-            field: 'updated_at'
-        }
-    },
-    {
-        freezeTableName: true,
-        underscored: true,
-        timestamps: true,
-        tableName: "user"
-    }
-);
+@Table({
+    tableName: "user"
+})
+export class User extends Model<UserEntity> {
+    @PrimaryKey
+    @Unique
+    @Column
+    userId!: string
 
-User.hasMany(Group, {
-    foreignKey: 'user_id'
-});
+    @Column
+    name!: string
 
-Group.belongsTo(User, {
-    foreignKey: 'user_id',
-    targetKey: 'user_id'
-});
+    @Column
+    email!: string
+
+    @Column
+    password!: string
+
+    @Column
+    lastname!: string
+}
 
 export default User;
