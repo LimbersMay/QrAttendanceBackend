9012c0a479c84ce7a0650d17c957c4527c3a752d
limberMay <limbermay1@gmail.com>
2023-01-26 16:55:57 -0600

    (qrAttendance/user) se hizo uso de Either y left para valdiar al usuario en passport

--- src/contexts/QrAttendance/auth/infrastructure/passport/config.ts
+++ src/contexts/QrAttendance/auth/infrastructure/passport/config.ts
@@ -2,11 +2,11 @@
 import {Strategy as LocalStrategy} from 'passport-local';
 import passport from "passport";
 import {EncryptService} from "../../../shared/application/services/encrypt.service";
-import {UserEntity} from "../../../user/domain/user.entity";
+import {UserEntity} from "../../../user/domain";
 import {UserService} from "../../../user/application/user.service";
 
-import {isLeft} from "fp-ts/Either";
-import {UserError} from "../../../user/application/exceptions/userError";
+import {isLeft, isRight} from "fp-ts/Either";
+import {UserError} from "../../../user/domain/errors/userError";
 
 declare global {
     namespace Express {
@@ -38,7 +38,7 @@ export class PassportLocalStrategy {
                     const user = await this.userService.findUserByEmail(email);
 
                     if (isLeft(user)) {
-                        return done(user.left, false)
+                        return done(UserError.INVALID_CREDENTIALS, false)
                     }
 
                     const isValidPassword = await this.hashService.compare(password, user.right.password);
@@ -53,13 +53,15 @@ export class PassportLocalStrategy {
         // {id: 1, name: Juan}
         // 1 -> Serialización, pasar de un objeto a un dato muy particular
         passport.serializeUser((user, done) => {
-            done(null, user.id);
+            done(null, user.userId);
         });
 
         // 1 -> {id: 1, name: Juan}. Deserializacion Pasar del identificador al objeto
         passport.deserializeUser(async (id: string, done) => {
             const user = await this.userService.findUserById(id);
-            done(null, user);
+            return (isRight(user))
+                ? done(user.right, null)
+                : done(user.left, null);
         });
     }
 
