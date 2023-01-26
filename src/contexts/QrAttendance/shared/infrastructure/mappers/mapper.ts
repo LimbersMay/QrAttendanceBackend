7abeaed0b9e6c5167197b449c8a29e26dd15c94a
limberMay <limbermay1@gmail.com>
2023-01-26 16:56:26 -0600

    (qrAttendance/user) función toPersistance del mapperService eliminada

--- src/contexts/QrAttendance/shared/infrastructure/mappers/mapper.ts
+++ src/contexts/QrAttendance/shared/infrastructure/mappers/mapper.ts
@@ -4,10 +4,6 @@ interface Mapper<S, T> {
     toDomain(entity: S[]): T[];
     toDomain(entityOrArray: S | S[]): T | T[];
 
-    toPersistance(entity: T): S;
-    toPersistance(entity: T[]): S[];
-    toPersistance(entityOrArray: T | T[]): S | S[];
-
     toDTO(entity: T): S;
     toDTO(entity: T[]): S[];
     toDTO(entityOrArray: T | T[]): S | S[];
@@ -16,7 +12,6 @@ interface Mapper<S, T> {
 export abstract class MapperService<S, T> implements Mapper<S, T> {
 
     protected abstract mapToDomain(entity: S): T;
-    protected abstract mapToPersistance(entity: T): S;
     protected abstract mapToDTO(entity: T): S;
     toDomain(entity: S): T;
     toDomain(entity: S[]): T[];
@@ -27,17 +22,6 @@ export abstract class MapperService<S, T> implements Mapper<S, T> {
         }
 
         return this.mapToDomain(entityOrArray);
-    }
-
-    toPersistance(entity: T): S;
-    toPersistance(entity: T[]): S[];
-    toPersistance(entityOrArray: T | T[]): S | S[] {
-
-        if (Array.isArray(entityOrArray)) {
-            return entityOrArray.map((item: T) => this.mapToPersistance(item));
-        }
-
-        return this.mapToPersistance(entityOrArray);
     }
 
     toDTO(entity: T): S;
