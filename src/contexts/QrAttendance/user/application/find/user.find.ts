83fb2ee4a3f6567c9b29ce6fed0810a66d15e556
limberMay <limbermay1@gmail.com>
2023-01-27 12:27:00 -0600

    (qrAttendance/user) caso de uso UserFinder agregado

---
+++ src/contexts/QrAttendance/user/application/find/user.find.ts
@@ -0,0 +1,28 @@
+import {UserRepository} from "../../domain";
+import {UserResponse} from "../responses/user.response";
+import {Either} from "../../../../shared/types/ErrorEither";
+import {UserError} from "../../domain/errors/userError";
+import {isRight, left, right} from "fp-ts/Either";
+
+export class UserFinder {
+    constructor(
+        private readonly repository: UserRepository,
+    ) {
+    }
+
+    execute(id: string): Promise<Either<UserError, UserResponse>> {
+        return this.repository.findUserById(id).then((user) => {
+            return isRight(user)
+                ? right(UserResponse.fromUser(user.right))
+                : left(UserError.USER_NOT_FOUND);
+        });
+    }
+
+    executeByEmail(email: string): Promise<Either<UserError, UserResponse>> {
+        return this.repository.findUserByEmail(email).then((user) => {
+            return isRight(user)
+                ? right(UserResponse.fromUser(user.right))
+                : left(UserError.USER_NOT_FOUND);
+        });
+    }
+}
