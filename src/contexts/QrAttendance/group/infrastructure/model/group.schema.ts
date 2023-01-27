b7b07224546a67983df9f063ec6ab56da9d70768
limberMay <limbermay1@gmail.com>
2023-01-26 21:09:45 -0600

    (qrAttendance/group) Schema Group inicializado

--- src/contexts/QrAttendance/group/infrastructure/model/group.schema.ts
+++ src/contexts/QrAttendance/group/infrastructure/model/group.schema.ts
@@ -1,10 +1,13 @@
-import {Table, Column, Model, PrimaryKey, Unique, ForeignKey} from "sequelize-typescript";
+import {Table, Column, Model, PrimaryKey, Unique, ForeignKey } from "sequelize-typescript";
 import {GroupEntity} from "../../domain/group.entity";
 import User from "../../../user/infrastructure/model/user.schema";
+import sequelize from "../../../../shared/infrastructure/db/mysql.connection";
+import {DataTypes} from "sequelize";
 
 @Table({
     tableName: "group"
 })
+
 class Group extends Model<GroupEntity> {
     @PrimaryKey
     @Unique
@@ -17,6 +20,41 @@ class Group extends Model<GroupEntity> {
 
     @Column
     name!: string;
+    @Column
+    createdAt!: Date;
+    @Column
+    updatedAt!: Date;
 }
+
+const groupAttributes = {
+    groupId: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false
+    },
+    userId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'userId'
+        }
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+}
+
+sequelize.addModels([Group]);
+Group.init(groupAttributes, {sequelize: sequelize, tableName: "group"});
 
 export default Group;
