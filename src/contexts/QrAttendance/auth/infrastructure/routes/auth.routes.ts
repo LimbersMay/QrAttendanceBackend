e33ed6641af782fd4823688486cbb05e47ec02e6
limberMay <limbermay1@gmail.com>
2023-01-27 09:41:00 -0600

    (qrAttendance/auth) dependencias para userService agregado al auth.routes.ts

--- src/contexts/QrAttendance/auth/infrastructure/routes/auth.routes.ts
+++ src/contexts/QrAttendance/auth/infrastructure/routes/auth.routes.ts
@@ -1,13 +1,26 @@
 import { Router} from "express";
 import {UserMapperService} from "../../../user/infrastructure/mappers/user.mapper";
 import {AuthController} from "../controller/auth.controller";
+import {UserService} from "../../../user/application/user.service";
+import {BcryptAdapter} from "../../../user/infrastructure/adapters/bcryptAdapter";
+import {UuidAdapter} from "../../../user/infrastructure/adapters/uuid.adapter";
+import {UserMysqlRepository} from "../../../user/infrastructure/repository/user.repository";
+import {AuthMiddleware} from "../middlewares";
 
 const router = Router();
 
-const userMapper = new UserMapperService();
-const authController = new AuthController(userMapper);
+const encryptService = new BcryptAdapter();
+const userMapperService = new UserMapperService();
+const uuidService = new UuidAdapter();
+const userRepository = new UserMysqlRepository();
+const userService = new UserService(userRepository, encryptService, userMapperService, uuidService);
+
+const authController = new AuthController(userMapperService, userService);
+const authMiddleware = new AuthMiddleware(userService);
 
 router.post("/login-local", authController.loginLocal);
+
+router.post("/register-local",[authMiddleware.emailExists], authController.registerLocal);
 
 router.post("/logout", authController.logout);
 
