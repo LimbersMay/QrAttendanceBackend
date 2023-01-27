709cc849f9c86be0b46d4d9bc4e392f5c81dbced
limberMay <limbermay1@gmail.com>
2023-01-27 09:39:43 -0600

    (qrAttendance/auth) Middleware de auth para verificar si un email existe o no creado

--- src/contexts/QrAttendance/auth/infrastructure/middlewares/auth.middleware.ts
+++ src/contexts/QrAttendance/auth/infrastructure/middlewares/auth.middleware.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import {AuthErrors} from "../../application/errors/auth.errors";
+import {isRight} from "fp-ts/Either";
+import {UserService} from "../../../user/application/user.service";
 
 export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
     if (req.isAuthenticated()) {
@@ -10,4 +12,21 @@ export const isAuthenticated = (req: Request, res: Response, next: NextFunction)
         ok: false,
         msg: AuthErrors.NOT_AUTHENTICATED
     })
+}
+
+export class AuthMiddleware {
+
+    constructor(
+        private readonly userService: UserService
+    ) {
+    }
+    public emailExists = async (req: Request, res: Response, next: NextFunction) => {
+
+        const { email } = req.body;
+        const exists = await this.userService.findUserByEmail(email);
+
+        return isRight(exists)
+            ? res.status(400).json({msg: AuthErrors.DUPLICATED_EMAIL})
+            : next();
+    }
 }
