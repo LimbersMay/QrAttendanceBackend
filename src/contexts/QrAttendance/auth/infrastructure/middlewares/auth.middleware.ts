f3aed8b5bcbfb2c1120feaf655a8e52bf3104c04
limberMay <limbermay1@gmail.com>
2023-01-27 12:23:19 -0600

    (qrAttendance/auth) dependencia de userService reemplaza por userFinder en el auth.middleware.ts

--- src/contexts/QrAttendance/auth/infrastructure/middlewares/auth.middleware.ts
+++ src/contexts/QrAttendance/auth/infrastructure/middlewares/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import {AuthErrors} from "../../application/errors/auth.errors";
+import {AuthError} from "../../application/errors/authError";
 import {isRight} from "fp-ts/Either";
-import {UserService} from "../../../user/application/user.service";
+import {UserFinder} from "../../../user/application/find/user.find";
 
 export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
     if (req.isAuthenticated()) {
@@ -10,23 +10,23 @@ export const isAuthenticated = (req: Request, res: Response, next: NextFunction)
 
     return res.status(401).json({
         ok: false,
-        msg: AuthErrors.NOT_AUTHENTICATED
+        msg: AuthError.NOT_AUTHENTICATED
     })
 }
 
 export class AuthMiddleware {
 
     constructor(
-        private readonly userService: UserService
+        private readonly userFinder: UserFinder
     ) {
     }
     public emailExists = async (req: Request, res: Response, next: NextFunction) => {
 
         const { email } = req.body;
-        const exists = await this.userService.findUserByEmail(email);
+        const exists = await this.userFinder.executeByEmail(email);
 
         return isRight(exists)
-            ? res.status(400).json({msg: AuthErrors.DUPLICATED_EMAIL})
+            ? res.status(400).json({msg: AuthError.DUPLICATED_EMAIL})
             : next();
     }
 }
