d4829f02b29fed2d82d24e088776c7f5331c7891
limberMay <limbermay1@gmail.com>
2023-01-27 22:35:44 -0600

    (qrAttendance/user) excepciones no controladas manejadas con el catch del caso de uso user.finder.ts

--- src/contexts/QrAttendance/user/application/useCases/user.finder.ts
+++ src/contexts/QrAttendance/user/application/useCases/user.finder.ts
@@ -7,15 +7,15 @@ import {isRight, left, right} from "fp-ts/Either";
 export class UserFinder {
     constructor(
         private readonly repository: UserRepository,
-    ) {
-    }
+    ) {}
 
     execute = (id: string): Promise<Either<UserError, UserResponse>> => {
         return this.repository.findUserById(id).then((user) => {
             return isRight(user)
                 ? right(UserResponse.fromUser(user.right))
                 : left(UserError.USER_NOT_FOUND);
-        });
+
+        }).catch(() => left(UserError.UNEXPECTED_ERROR));
     }
 
     executeByEmail = (email: string): Promise<Either<UserError, UserResponse>> => {
