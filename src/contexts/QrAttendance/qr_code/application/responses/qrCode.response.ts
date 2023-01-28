8fc66c1bf9099bf7b3aa3691355fa159de4d154b
limberMay <limbermay1@gmail.com>
2023-01-28 11:59:40 -0600

    (qrAttendance) objeto QrCode response creado para mapear un objeto de dominio a DTO

---
+++ src/contexts/QrAttendance/qr_code/application/responses/qrCode.response.ts
@@ -0,0 +1,19 @@
+import {QrCodeEntity} from "../../domain/qrCode.entity";
+
+export class QrCodeResponse {
+    id: string;
+    name: string;
+    enabled: boolean;
+    createdAt?: Date;
+
+    constructor(id: string, name: string, enabled: boolean, createdAt?: Date) {
+        this.id = id;
+        this.name = name;
+        this.createdAt = createdAt;
+        this.enabled = enabled;
+    }
+
+    static fromQrCode(qrCode: QrCodeEntity): QrCodeResponse {
+        return new QrCodeResponse(qrCode.qrId, qrCode.name, qrCode.enabled, qrCode.createdAt);
+    }
+}
