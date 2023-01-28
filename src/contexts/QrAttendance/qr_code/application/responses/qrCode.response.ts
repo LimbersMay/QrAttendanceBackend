a2d1ca9c5124ccea34a7141fce3c15913f49080a
limberMay <limbermay1@gmail.com>
2023-01-28 16:44:14 -0600

    (qrAttendance/qrCode) groupId agregado al QrCodeResponse

--- src/contexts/QrAttendance/qr_code/application/responses/qrCode.response.ts
+++ src/contexts/QrAttendance/qr_code/application/responses/qrCode.response.ts
@@ -2,19 +2,21 @@ import {QrCodeEntity} from "../../domain/qrCode.entity";
 
 export class QrCodeResponse {
     id: string;
+    groupId: string;
     name: string;
     enabled: boolean;
     createdAt?: Date;
 
-    constructor(id: string, name: string, enabled: boolean, createdAt?: Date) {
+    constructor(id: string, groupId: string, name: string, enabled: boolean, createdAt?: Date) {
         this.id = id;
+        this.groupId = groupId;
         this.name = name;
         this.createdAt = createdAt;
         this.enabled = enabled;
     }
 
     static fromQrCode(qrCode: QrCodeEntity): QrCodeResponse {
-        return new QrCodeResponse(qrCode.qrId, qrCode.name, qrCode.enabled, qrCode.createdAt);
+        return new QrCodeResponse(qrCode.qrId, qrCode.groupId, qrCode.name, qrCode.enabled, qrCode.createdAt);
     }
 
     static fromQrCodes(qrCodes: QrCodeEntity[]): QrCodeResponse[] {
