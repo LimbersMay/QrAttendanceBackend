37d5526fac2102a841193cdcf0d8a303bf5098b6
limberMay <limbermay1@gmail.com>
2023-01-28 17:52:38 -0600

    (qrAttendance/registry) campos createdAt y updatedAt marcados como opcionales en el registry.entity.ts

--- src/contexts/QrAttendance/registry/domain/registry.entity.ts
+++ src/contexts/QrAttendance/registry/domain/registry.entity.ts
@@ -6,6 +6,6 @@ export interface RegistryEntity {
     name: string,
     firstSurname: string,
     secondSurname: string,
-    createdAt: Date,
-    updatedAt: Date
+    createdAt?: Date,
+    updatedAt?: Date
 }
