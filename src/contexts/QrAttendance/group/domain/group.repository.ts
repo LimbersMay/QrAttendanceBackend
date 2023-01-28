5aa288cc52ceae9279444087c2b556d86ea6a387
limberMay <limbermay1@gmail.com>
2023-01-28 10:43:58 -0600

    (qrAttendance/group) se devolvió un groupEntity en el método createGroup del group.repository.ts y una entidad GroupEntity

--- src/contexts/QrAttendance/group/domain/group.repository.ts
+++ src/contexts/QrAttendance/group/domain/group.repository.ts
@@ -5,7 +5,7 @@ import {Either} from "../../../shared/types/ErrorEither";
 export interface GroupRepository {
     findGroupById(groupId: string, userId: string): Promise<Either<GroupError, GroupEntity>>;
     findGroupsByUserId(userId: string): Promise<Either<GroupError, GroupEntity[]>>;
-    createGroup(user: GroupEntity): Promise<Either<GroupError, GroupEntity>>;
+    createGroup(group: GroupEntity): Promise<GroupEntity>;
     deleteGroup(groupId: string, userId: string): Promise<Either<GroupError, number>>;
     updateGroup(groupId: string, userId: string, fields: Record<string, any>): Promise<Either<GroupError, number>>;
 }
