3c1933dd3d49614d629e1dba72d396d3fcb086e4
limberMay <limbermay1@gmail.com>
2023-01-28 10:35:58 -0600

    (qrAttendance/group) se recibió el nombre y userId del group en lugar de un groupEntity completo

--- src/contexts/QrAttendance/group/application/useCases/create/group.creator.ts
+++ src/contexts/QrAttendance/group/application/useCases/create/group.creator.ts
@@ -1,22 +1,27 @@
-import {GroupEntity} from "../../../domain/group.entity";
 import {GroupError} from "../../errors/group.errors";
 import {Either} from "../../../../../shared/types/ErrorEither";
 import {GroupRepository} from "../../../domain/group.repository";
 import {GroupResponse} from "../../responses/group.response";
-import { left, right, fold} from "fp-ts/Either";
+import { left, right} from "fp-ts/Either";
+import {GroupValue} from "../../../domain/group.value";
+import {UUIDGenerator} from "../../../../shared/application/services/UUIDGenerator";
 
 export class GroupCreator {
     constructor(
-        private readonly groupRepository: GroupRepository
+        private readonly groupRepository: GroupRepository,
+        private readonly UUIDGenerator: UUIDGenerator
     ) {}
 
-    execute = (group: GroupEntity): Promise<Either<GroupError, GroupResponse>> => {
-        return this.groupRepository.createGroup(group).then(group => {
-            return fold(
-                (error: GroupError) => left(error),
-                (group: GroupEntity) => right(GroupResponse.fromGroup(group))
-            )(group);
+    execute = (name: string, userId: string): Promise<Either<GroupError, GroupResponse>> => {
 
+        const group = GroupValue.create({
+            groupId: this.UUIDGenerator.random(),
+            name,
+            userId
+        });
+
+        return this.groupRepository.createGroup(group).then(group => {
+            return right(GroupResponse.fromGroup(group));
         }).catch(() => left(GroupError.GROUP_CANNOT_BE_CREATED));
     }
 }
