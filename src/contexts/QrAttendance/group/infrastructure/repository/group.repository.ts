9fd7acaf82f0e604e325c451f5d97e902640a70a
limberMay <limbermay1@gmail.com>
2023-01-28 00:50:44 -0600

    (qrAttendance/group) try-catch eliminado del GroupRepository

--- src/contexts/QrAttendance/group/infrastructure/repository/group.repository.ts
+++ src/contexts/QrAttendance/group/infrastructure/repository/group.repository.ts
@@ -3,112 +3,92 @@ import {GroupEntity} from "../../domain/group.entity";
 import {Either} from "../../../../shared/types/ErrorEither";
 import {GroupError} from "../../application/errors/group.errors";
 import {left, right} from "fp-ts/Either";
+import {GroupRepository} from "../../domain/group.repository";
 
-export class GroupRepository {
+export class GroupMysqlRepository implements GroupRepository {
 
-    public async findGroupById (groupId: string, userId: string): Promise<Either<GroupError, GroupEntity>> {
+    public async findGroupById(groupId: string, userId: string): Promise<Either<GroupError, GroupEntity>> {
 
-        try {
-            const group = await Group.findOne({
-                where: {
-                    groupId,
-                    userId
-                }
-            });
 
-            return (group)
-                ? right({
-                    groupId: group.groupId,
-                    name: group.name,
-                    userId: group.userId,
-                    createdAt: group.createdAt,
-                    updatedAt: group.updatedAt
-                })
-                : left(GroupError.GROUP_NOT_FOUND);
+        const group = await Group.findOne({
+            where: {
+                groupId,
+                userId
+            }
+        });
 
-        } catch (error) {
-            return left(GroupError.GROUP_GET_FAILED);
-        }
-    }
-
-    public async findGroupsByUserId (userId: string): Promise<Either<GroupError, GroupEntity[]>> {
-
-        try {
-            const groups = await Group.findAll({
-                where: {
-                    userId
-                }
-            });
-
-            return right(groups.map(group => ({
+        return (group)
+            ? right({
                 groupId: group.groupId,
                 name: group.name,
                 userId: group.userId,
                 createdAt: group.createdAt,
                 updatedAt: group.updatedAt
-            })));
+            })
+            : left(GroupError.GROUP_NOT_FOUND);
 
-        } catch (error) {
-            return left(GroupError.GROUP_GET_FAILED);
-        }
+
     }
 
-    public async createGroup (user: GroupEntity): Promise<Either<GroupError, GroupEntity>> {
-        try {
-            const groupCreated = await Group.create(user);
+    public async findGroupsByUserId(userId: string): Promise<Either<GroupError, GroupEntity[]>> {
 
-            return right({
-                groupId: groupCreated.groupId,
-                name: groupCreated.name,
-                userId: groupCreated.userId,
-                createdAt: groupCreated.createdAt,
-                updatedAt: groupCreated.updatedAt
-            });
 
-        } catch (error) {
-            return left(GroupError.GROUP_CREATE_FAILED);
-        }
+        const groups = await Group.findAll({
+            where: {
+                userId
+            }
+        });
+
+        return right(groups.map(group => ({
+            groupId: group.groupId,
+            name: group.name,
+            userId: group.userId,
+            createdAt: group.createdAt,
+            updatedAt: group.updatedAt
+        })));
     }
 
-    public async deleteGroup (groupId: string, userId: string): Promise<Either<GroupError, number>> {
+    public async createGroup(user: GroupEntity): Promise<Either<GroupError, GroupEntity>> {
 
-        try {
-            const rowsDestroyed = await Group.destroy({
+        const groupCreated = await Group.create(user);
+
+        return right({
+            groupId: groupCreated.groupId,
+            name: groupCreated.name,
+            userId: groupCreated.userId,
+            createdAt: groupCreated.createdAt,
+            updatedAt: groupCreated.updatedAt
+        });
+    }
+
+    public async deleteGroup(groupId: string, userId: string): Promise<Either<GroupError, number>> {
+        const rowsDestroyed = await Group.destroy({
+            where: {
+                groupId,
+                userId
+            }
+        });
+
+        return (rowsDestroyed > 0)
+            ? right(rowsDestroyed)
+            : left(GroupError.GROUP_NOT_FOUND);
+    }
+
+    async updateGroup(groupId: string, userId: string, fields: Record<string, any>): Promise<Either<GroupError, number>> {
+
+        const rowsUpdated = await Group.update(
+            {
+                ...fields
+            },
+            {
                 where: {
                     groupId,
                     userId
                 }
             });
 
-            return (rowsDestroyed > 0)
-                ? right(rowsDestroyed)
-                : left(GroupError.GROUP_NOT_FOUND);
-
-        } catch (error) {
-            return left(GroupError.GROUP_DELETE_FAILED);
-        }
-    }
-
-    async updateGroup(groupId: string, userId: string, fields: Record<string, any>): Promise<Either<GroupError, number>> {
-
-        try {
-            const rowsUpdated = await Group.update(
-                {
-                    ...fields
-                },
-                {
-                    where: {
-                        groupId,
-                        userId
-                    }
-                });
-
-            return (rowsUpdated[0] > 0)
-                ? right(rowsUpdated[0])
-                : left(GroupError.GROUP_NOT_FOUND);
-
-        } catch (error) {
-            return left(GroupError.GROUP_UPDATE_FAILED);
-        }
+        return (rowsUpdated[0] > 0)
+            ? right(rowsUpdated[0])
+            : left(GroupError.GROUP_NOT_FOUND);
     }
 }
