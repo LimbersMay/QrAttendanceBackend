5b7b4402799d4dac5c2ef7923cbf5cd155c5d816
limberMay <limbermay1@gmail.com>
2023-01-28 12:24:23 -0600

    (qrAttendance/qrCode) método para mapear un arreglo de códigos qr a DTO en qrCode.response.ts

--- src/contexts/QrAttendance/qr_code/application/responses/qrCode.response.ts
+++ src/contexts/QrAttendance/qr_code/application/responses/qrCode.response.ts
@@ -16,4 +16,8 @@ export class QrCodeResponse {
     static fromQrCode(qrCode: QrCodeEntity): QrCodeResponse {
         return new QrCodeResponse(qrCode.qrId, qrCode.name, qrCode.enabled, qrCode.createdAt);
     }
+
+    static fromQrCodes(qrCodes: QrCodeEntity[]): QrCodeResponse[] {
+        return qrCodes.map(qrCode => QrCodeResponse.fromQrCode(qrCode));
+    }
 }
