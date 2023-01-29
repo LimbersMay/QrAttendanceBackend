82c7a3f2f740e35796b62863cc51d9c688015e71
limberMay <limbermay1@gmail.com>
2023-01-28 18:45:49 -0600

    (qrAttendance/registry) modelo Registry inicializado

--- src/contexts/shared/infrastructure/db/mysql.connection.ts
+++ src/contexts/shared/infrastructure/db/mysql.connection.ts
@@ -2,6 +2,7 @@ import {DataType, Sequelize} from 'sequelize-typescript'
 import User from "../../../QrAttendance/user/infrastructure/model/user.schema";
 import Group from "../../../QrAttendance/group/infrastructure/model/group.schema";
 import QrCode from "../../../QrAttendance/qr_code/infrastructure/models/qrCode.schema";
+import Registry from "../../../QrAttendance/registry/infrastructure/model/registry.schema";
 
 const sequelize = new Sequelize('QrAttendance', 'limber', '15891', {
     host: 'localhost',
@@ -110,10 +111,54 @@ const qrCodeAttributes = {
     }
 }
 
-sequelize.addModels([User, Group, QrCode]);
+const registryAttributes = {
+    registryId: {
+        type: DataType.STRING,
+        primaryKey: true,
+        unique: true,
+        allowNull: false
+    },
+    qrCodeId: {
+        type: DataType.STRING,
+        allowNull: false,
+        references: {
+            model: QrCode,
+            key: 'qrId'
+        }
+    },
+    ownerId: {
+        type: DataType.STRING,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'userId'
+        }
+    },
+    name: {
+        type: DataType.STRING,
+        allowNull: false
+    },
+    firstSurname: {
+        type: DataType.STRING,
+        allowNull: false
+    },
+    secondSurname: {
+        type: DataType.STRING,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataType.DATE
+    },
+    updatedAt: {
+        type: DataType.DATE
+    }
+}
+
+sequelize.addModels([User, Group, QrCode, Registry]);
 
 User.init(userAttributes, {sequelize: sequelize, tableName: "user"});
 Group.init(groupAttributes, {sequelize: sequelize, tableName: "group"});
 QrCode.init(qrCodeAttributes, {sequelize: sequelize, tableName: "qrCode"});
+Registry.init(registryAttributes, {sequelize: sequelize, tableName: "registry"});
 
 export default sequelize;
