ff0894015a8bde1c819ba79bb0986b9705050efe
limberMay <limbermay1@gmail.com>
2023-01-30 11:57:53 -0600

    (src/apps/qrAttendance) modulo de contenedores de group agregado al container principal

--- src/apps/QrAttendance/dependency-injection/container.ts
+++ src/apps/QrAttendance/dependency-injection/container.ts
@@ -1,11 +1,15 @@
 import {Container} from "inversify";
 import { userModule } from "./user/user.module";
 import {UserController} from "../../../contexts/QrAttendance/user/infrastructure/controller";
+import {groupModule} from "./group/group.module";
+import {GroupController} from "../../../contexts/QrAttendance/group/infrastructure/controller/group.controller";
 
 const container = new Container();
 
 userModule(container);
+groupModule(container);
 
 const UserControllerInjected = container.get<UserController>(UserController);
+    const GroupControllerInjected = container.get<GroupController>(GroupController);
 
-export { UserControllerInjected };
+export { UserControllerInjected, GroupControllerInjected };
