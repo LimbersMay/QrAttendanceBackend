38efce4f4eef5ed884c97c2fe8f2ed5b7dad7ce6
limberMay <limbermay1@gmail.com>
2023-01-30 12:25:43 -0600

    (src/contexts/qrAttendance/group) controller importado desde la exportación del container en lugar de crear instancias de clase

--- src/contexts/QrAttendance/qr_code/infrastructure/routes/qrCode.router.ts
+++ src/contexts/QrAttendance/qr_code/infrastructure/routes/qrCode.router.ts
@@ -1,22 +1,7 @@
 import {Router} from "express";
-import {QrCodeCreator, QrCodeDeleter, QrCodeFinder, QrCodeUpdater} from "../../application/useCases";
-import {QrCodeMysqlRepository} from "../repository/qrCode.repository";
-import {UuidAdapter} from "../adapters";
-import { QrCodeController } from "../controllers";
+import {QrCodeControllerInjected as qrCodeController} from "../../../../../apps/QrAttendance/dependency-injection/container";
 
 const router = Router();
-
-// inicializamos casos de uso
-const qrCodeMysqlRepository = new QrCodeMysqlRepository();
-const uuidAdapter = new UuidAdapter();
-
-const qrCodeFinder = new QrCodeFinder(qrCodeMysqlRepository);
-const qrCodeUpdater = new QrCodeUpdater(qrCodeMysqlRepository);
-const qrCodeDeleter = new QrCodeDeleter(qrCodeMysqlRepository);
-const qrCodeCreator = new QrCodeCreator(qrCodeMysqlRepository, uuidAdapter);
-
-// inicializamos controladores
-const qrCodeController = new QrCodeController(qrCodeFinder, qrCodeUpdater, qrCodeDeleter, qrCodeCreator);
 
 // definimos rutas
 router.get('/all', qrCodeController.findByUserId);
