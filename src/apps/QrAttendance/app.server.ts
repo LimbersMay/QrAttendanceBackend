b24fbaf50000f0d5fcd29b83acd987f1e6b6bf7f
limberMay <limbermay1@gmail.com>
2023-01-30 13:29:05 -0600

    (src/contexts/qrAttendance/auth) clase PassportLocalStrategy usada directamente con sus dependencias resueltas en app.server.ts

--- src/apps/QrAttendance/app.server.ts
+++ src/apps/QrAttendance/app.server.ts
@@ -1,11 +1,10 @@
 import { Application } from "express";
+import session from "express-session";
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import db from "../../contexts/shared/infrastructure/db/mysql.connection"
-import {PassportLocalStrategy} from "../../contexts/QrAttendance/auth/infrastructure/passport/config";
-import {UserMysqlRepository} from "../../contexts/QrAttendance/user/infrastructure/repository/user.repository";
-import {BcryptAdapter} from "../../contexts/QrAttendance/user/infrastructure/adapters";
 
 // routes
 import authRoutes from "../../contexts/QrAttendance/auth/infrastructure/routes/auth.routes";
@@ -14,30 +13,19 @@ import groupRoutes from "../../contexts/QrAttendance/group/infrastructure/routes
 import qrCodeRoutes from "../../contexts/QrAttendance/qr_code/infrastructure/routes/qrCode.router";
 import registryRoutes from "../../contexts/QrAttendance/registry/infrastructure/routes/registry.router";
 
-import cookieParser from "cookie-parser";
-import session from "express-session";
-import {AuthenticateUser} from "../../contexts/QrAttendance/auth/application/authentication/auth";
-import {UserFinder} from "../../contexts/QrAttendance/user/application/useCases";
+import {AuthPassportStrategyInjected as PassportLocalStrategy} from "./dependency-injection/container";
 
 export class Server {
     public app: Application;
     public port: number;
     public appRoutes: Record<string, string>;
-    public authService: PassportLocalStrategy;
 
     constructor() {
 
         this.app = express();
         this.port = parseInt(process.env.PORT ?? "3000")
 
-        const userRepo = new UserMysqlRepository();
-        const encryptService = new BcryptAdapter();
-
-        const authenticateUser = new AuthenticateUser(userRepo, encryptService);
-        const userFinder = new UserFinder(userRepo);
-
-        this.authService = new PassportLocalStrategy(authenticateUser, userFinder);
-        this.authService.init();
+        PassportLocalStrategy.init();
 
         // routes
         this.appRoutes = {
@@ -82,8 +70,8 @@ export class Server {
         }));
 
         // passport
-        this.app.use(this.authService.initialize());
-        this.app.use(this.authService.session());
+        this.app.use(PassportLocalStrategy.initialize());
+        this.app.use(PassportLocalStrategy.session());
     }
 
     public routes() {
