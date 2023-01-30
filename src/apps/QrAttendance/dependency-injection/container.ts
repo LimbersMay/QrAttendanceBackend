6e25f864c28324a398ba585f139113f6ec1a7d81
limberMay <limbermay1@gmail.com>
2023-01-30 12:45:07 -0600

    (src/apps/qrAttendance/DI) modulo de registro importado y se exportó su dependencia resuelta

--- src/apps/QrAttendance/dependency-injection/container.ts
+++ src/apps/QrAttendance/dependency-injection/container.ts
@@ -5,15 +5,27 @@ import {groupModule} from "./group/group.module";
 import {GroupController} from "../../../contexts/QrAttendance/group/infrastructure/controller/group.controller";
 import {qrCodeModule} from "./qrCode/qrCode.module";
 import {QrCodeController} from "../../../contexts/QrAttendance/qr_code/infrastructure/controllers";
+import {registryModule} from "./registry/registry.module";
+import {
+    RegistryController
+} from "../../../contexts/QrAttendance/registry/infrastructure/controller/registry.controller";
 
 const container = new Container();
 
 userModule(container);
 groupModule(container);
 qrCodeModule(container);
+registryModule(container);
 
 const UserControllerInjected = container.get<UserController>(UserController);
 const GroupControllerInjected = container.get<GroupController>(GroupController);
 const QrCodeControllerInjected = container.get<QrCodeController>(QrCodeController);
 
-export { UserControllerInjected, GroupControllerInjected, QrCodeControllerInjected };
+const RegistryControllerInjected = container.get<RegistryController>(RegistryController);
+
+export {
+    UserControllerInjected,
+    GroupControllerInjected,
+    QrCodeControllerInjected,
+    RegistryControllerInjected
+};
