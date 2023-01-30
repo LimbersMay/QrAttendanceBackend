cfc033d9544c1c10b3b5320321494bdd436ccc6e
limberMay <limbermay1@gmail.com>
2023-01-30 12:27:39 -0600

    (src/apps/qrAttendance/DI) módulo de qrCode importado y exportado el controlador

--- src/apps/QrAttendance/dependency-injection/container.ts
+++ src/apps/QrAttendance/dependency-injection/container.ts
@@ -3,13 +3,17 @@ import { userModule } from "./user/user.module";
 import {UserController} from "../../../contexts/QrAttendance/user/infrastructure/controller";
 import {groupModule} from "./group/group.module";
 import {GroupController} from "../../../contexts/QrAttendance/group/infrastructure/controller/group.controller";
+import {qrCodeModule} from "./qrCode/qrCode.module";
+import {QrCodeController} from "../../../contexts/QrAttendance/qr_code/infrastructure/controllers";
 
 const container = new Container();
 
 userModule(container);
 groupModule(container);
+qrCodeModule(container);
 
 const UserControllerInjected = container.get<UserController>(UserController);
-    const GroupControllerInjected = container.get<GroupController>(GroupController);
+const GroupControllerInjected = container.get<GroupController>(GroupController);
+const QrCodeControllerInjected = container.get<QrCodeController>(QrCodeController);
 
-export { UserControllerInjected, GroupControllerInjected };
+export { UserControllerInjected, GroupControllerInjected, QrCodeControllerInjected };
