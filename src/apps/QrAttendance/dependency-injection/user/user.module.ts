38f8fb255d5f5c972c00ba13255b866b48658296
limberMay <limbermay1@gmail.com>
2023-01-30 12:00:57 -0600

    (src/apps/qrAttendance/user) tipado de bind aplicado a los casos de uso

--- src/apps/QrAttendance/dependency-injection/user/user.module.ts
+++ src/apps/QrAttendance/dependency-injection/user/user.module.ts
@@ -1,6 +1,6 @@
 import {Container} from "inversify";
-
 import {TYPES} from "./types";
+
 import {UserRepository} from "../../../../contexts/QrAttendance/user/domain";
 import { UUIDGenerator } from "../../../../contexts/QrAttendance/shared/application/services/UUIDGenerator";
 import { PasswordHasher } from "../../../../contexts/QrAttendance/shared/application/services/encrypt.service";
@@ -15,14 +15,14 @@ import {
 import {UserController} from "../../../../contexts/QrAttendance/user/infrastructure/controller";
 
 export const userModule = (container: Container) => {
-    container.bind(UserController).toSelf();
+    container.bind<UserController>(UserController).toSelf();
 
-    container.bind(UserCreator).toSelf();
-    container.bind(UserUpdater).toSelf();
-    container.bind(UserDeleter).toSelf();
-    container.bind(UserFinder).toSelf();
+    container.bind<UserCreator>(UserCreator).toSelf();
+    container.bind<UserUpdater>(UserUpdater).toSelf();
+    container.bind<UserDeleter>(UserDeleter).toSelf();
+    container.bind<UserFinder>(UserFinder).toSelf();
 
     container.bind<UserRepository>(TYPES.UserRepository).to(UserMysqlRepository);
-    container.bind<UUIDGenerator>(TYPES.UUIDGenerator).to(UuidAdapter);
+    container.bind<UUIDGenerator>(TYPES.UserUUIDGenerator).to(UuidAdapter);
     container.bind<PasswordHasher>(TYPES.PasswordHasher).to(BcryptAdapter);
 }
