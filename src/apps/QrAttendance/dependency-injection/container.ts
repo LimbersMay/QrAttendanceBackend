8df382df676510061b2bd9a1d8b4be94192ce83b
limberMay <limbermay1@gmail.com>
2023-01-30 15:28:58 -0600

    (src/apps/qrAttendance/DI) se reexportaron los controladores habituales para evitar romper la aplicación

--- src/apps/QrAttendance/dependency-injection/container.ts
+++ src/apps/QrAttendance/dependency-injection/container.ts
@@ -1,10 +1,16 @@
 import {Container} from "inversify";
 import { userModule } from "./user/user.module";
 import {groupModule} from "./group/group.module";
+import {GroupController} from "../../../contexts/QrAttendance/group/infrastructure/controller/group.controller";
 import {qrCodeModule} from "./qrCode/qrCode.module";
+import {QrCodeController} from "../../../contexts/QrAttendance/qr_code/infrastructure/controllers";
 import {registryModule} from "./registry/registry.module";
+import {
+    RegistryController
+} from "../../../contexts/QrAttendance/registry/infrastructure/controller/registry.controller";
 import {authModule} from "./auth/auth.module";
-
+import {AuthController} from "../../../contexts/QrAttendance/auth/infrastructure/controller/auth.controller";
+import {AuthMiddleware} from "../../../contexts/QrAttendance/auth/infrastructure/middlewares";
 import {PassportLocalStrategy} from "../../../contexts/QrAttendance/auth/infrastructure/passport/config";
 
 const container = new Container();
@@ -15,9 +21,20 @@ groupModule(container);
 qrCodeModule(container);
 registryModule(container);
 
+const AuthControllerInjected = container.get<AuthController>(AuthController);
+const AuthMiddlewareInjected = container.get<AuthMiddleware>(AuthMiddleware);
 const AuthPassportStrategyInjected = container.get<PassportLocalStrategy>(PassportLocalStrategy);
+const GroupControllerInjected = container.get<GroupController>(GroupController);
+const QrCodeControllerInjected = container.get<QrCodeController>(QrCodeController);
+
+const RegistryControllerInjected = container.get<RegistryController>(RegistryController);
 
 export {
     container,
+    GroupControllerInjected,
+    QrCodeControllerInjected,
+    RegistryControllerInjected,
+    AuthControllerInjected,
+    AuthMiddlewareInjected,
     AuthPassportStrategyInjected
 };
