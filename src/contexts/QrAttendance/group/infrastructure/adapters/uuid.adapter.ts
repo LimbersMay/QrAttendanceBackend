62db1e8337b24c970ac537b80b9e9d5517ff9c35
limberMay <limbermay1@gmail.com>
2023-01-30 12:01:21 -0600

    (src/contexts/qrAttendance/user) adapter marcado como injectable

--- src/contexts/QrAttendance/group/infrastructure/adapters/uuid.adapter.ts
+++ src/contexts/QrAttendance/group/infrastructure/adapters/uuid.adapter.ts
@@ -1,7 +1,9 @@
 import {UUIDGenerator} from "../../../shared/application/services/UUIDGenerator";
 
 import { v4 as uuid } from 'uuid';
+import {injectable} from "inversify";
 
+@injectable()
 export class UuidAdapter implements UUIDGenerator {
     random(): string {
         return uuid();
