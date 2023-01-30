aac14db542bbd158bd3f25bb102afa03ff02712d
limberMay <limbermay1@gmail.com>
2023-01-30 15:30:17 -0600

    (src/contexts/qrAttendance/user) rutas de los controladores definidos dentro del controlador user.controller.ts con routing-controllers

--- src/contexts/QrAttendance/user/infrastructure/controller/user.controller.ts
+++ src/contexts/QrAttendance/user/infrastructure/controller/user.controller.ts
@@ -1,10 +1,12 @@
 import {UserFinder, UserDeleter, UserUpdater } from "../../application/useCases";
-import {Request, Response} from "express";
+import {Request} from "express";
 import {isRight} from "fp-ts/Either";
 import {ResponseEntity} from "../../../../shared/infrastructure/entities/response.entity";
 import {UserError} from "../../domain/errors/userError";
 import {injectable} from "inversify";
+import {Body, Delete, Get, JsonController, Put, QueryParam, Req} from "routing-controllers";
 
+@JsonController('/user')
 @injectable()
 export class UserController {
     constructor(
@@ -13,34 +15,41 @@ export class UserController {
         private readonly userDeleter: UserDeleter
     ) {}
 
-    public getUserById = async ({query}: Request, res: Response) => {
-        const {userId = ''} = query;
+    @Get('/')
+    public async getUserById (@QueryParam("userId", {required: true}) userId: string) {
 
-        const user = await this.userFinder.execute(`${userId}`);
+        const user = await this.userFinder.execute(userId);
 
-        if (isRight(user)) return ResponseEntity.ok().body(user.right).send(res);
+        if (isRight(user))
+            return ResponseEntity
+                .body(user.right)
+                .buid();
 
-        return this.handleError(res, user.left);
+        return this.handleError(user.left);
     }
 
-    public delete = async(req: Request, res: Response) => {
+    @Delete('/delete')
+    public async delete (@Req() req: Request) {
 
-        if (!req.user) return res.status(400).json({ msg: 'NOT_AUTHENTICATED' });
+        if (!req.user) return ResponseEntity.status(400).body('NOT_AUTHENTICATED').buildError();
 
         const { id: userId } = req.user;
         const user = await this.userDeleter.execute(userId);
 
-        if (isRight(user)) return ResponseEntity.ok().body(user.right).send(res);
+        if (isRight(user))
+            return ResponseEntity
+                .body(user.right)
+                .buid();
 
-        return this.handleError(res, user.left);
+        return this.handleError(user.left);
     }
 
-    public update = async(req: Request, res: Response) => {
+    @Put('/update')
+    public async update(@Req() req: Request, @Body() {fields}: any) {
 
-        if (!req.user) return res.status(400).json({ msg: 'NOT_AUTHENTICATED' });
+        if (!req.user) return ResponseEntity.status(400).body('NOT_AUTHENTICATED').buildError();
 
         const { id: userId = '' } = req.user;
-        const {fields} = req.body;
 
         const expectedFields = {
             name: fields.name,
@@ -50,47 +59,51 @@ export class UserController {
         }
 
         const user = await this.userUpdater.execute(expectedFields, userId);
-        if (isRight(user)) return ResponseEntity.ok().body(user.right).send(res);
+        if (isRight(user))
+            return ResponseEntity
+                .body(user.right)
+                .buid();
 
-       return this.handleError(res, user.left);
+       return this.handleError(user.left);
     }
 
-    private handleError = (res: Response, error: UserError) => {
+    private handleError (error: UserError) {
         switch (error) {
             case UserError.USER_NOT_FOUND:
                 return ResponseEntity
                     .status(404)
                     .body(error)
-                    .send(res);
+                    .buildError();
 
             case UserError.USER_CANNOT_BE_FOUND:
                 return ResponseEntity
                     .status(500)
                     .body(error)
-                    .send(res);
+                    .buildError();
 
                 case UserError.USER_CANNOT_BE_DELETED:
                 return ResponseEntity
                     .status(500)
                     .body(error)
-                    .send(res);
+                    .buildError();
 
                 case UserError.USER_CANNOT_BE_UPDATED:
                 return ResponseEntity
                     .status(500)
                     .body(error)
-                    .send(res);
+                    .buildError();
 
                 case UserError.USER_CANNOT_BE_CREATED:
                 return ResponseEntity
                     .status(500)
                     .body(error)
-                    .send(res);
+                    .buildError();
+
             default:
                 return ResponseEntity
                     .status(500)
-                    .body('INTERNAL_SERVER_ERROR')
-                    .send(res);
+                    .body(error)
+                    .buildError();
         }
     }
 }
