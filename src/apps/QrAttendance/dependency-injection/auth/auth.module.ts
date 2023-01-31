ee607e3cd30139c78293e32b6cd6f7ebd3ecc71e
limberMay <limbermay1@gmail.com>
2023-01-30 22:01:13 -0600

    (src/apps/qrAttendance/DI) middlewares de autenticación registrados en el modulo de auth

--- src/apps/QrAttendance/dependency-injection/auth/auth.module.ts
+++ src/apps/QrAttendance/dependency-injection/auth/auth.module.ts
@@ -1,10 +1,30 @@
 import {Container} from "inversify";
 import {AuthController} from "../../../../contexts/QrAttendance/auth/infrastructure/controller/auth.controller";
-import {AuthMiddleware} from "../../../../contexts/QrAttendance/auth/infrastructure/middlewares";
+import {
+    AuthMiddleware, EmailExists,
+    IsAuthenticated,
+    Logout
+} from "../../../../contexts/QrAttendance/auth/infrastructure/middlewares";
 import {PassportLocalStrategy} from "../../../../contexts/QrAttendance/auth/infrastructure/passport/config";
 import {AuthenticateUser} from "../../../../contexts/QrAttendance/auth/application/authentication/auth";
+import {
+    Authenticate,
+    InvalidCredentialsHandler
+} from "../../../../contexts/QrAttendance/auth/infrastructure/middlewares/providers.middleware";
 
 export const authModule = (container: Container) => {
+
+    // middlewares
+    container.bind<InvalidCredentialsHandler>(InvalidCredentialsHandler).toSelf();
+    container.bind<IsAuthenticated>(IsAuthenticated).toSelf();
+    container.bind<EmailExists>(EmailExists).toSelf();
+    container.bind<Logout>(Logout).toSelf();
+
+    // middlewares error handlers
+
+    // auth providers
+    container.bind<Authenticate>(Authenticate).toSelf();
+
     container.bind<AuthController>(AuthController).toSelf();
     container.bind<AuthMiddleware>(AuthMiddleware).toSelf();
 
