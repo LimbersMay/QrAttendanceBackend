62bccb1ae300b229ec95b54c18e088b9d96fea2f
limberMay <limbermay1@gmail.com>
2023-01-30 22:00:28 -0600

    (src/contexts/qrAttendance/auth) middleware EmailExists creado para verificar si un email existe antes de registrarlo

--- src/contexts/QrAttendance/auth/infrastructure/middlewares/auth.middleware.ts
+++ src/contexts/QrAttendance/auth/infrastructure/middlewares/auth.middleware.ts
@@ -4,6 +4,7 @@ import {isRight} from "fp-ts/Either";
 import {AuthError} from "../../application/errors/authError";
 import {UserFinder} from "../../../user/application/useCases";
 import {ResponseEntity} from "../../../../shared/infrastructure/entities/response.entity";
+import {ExpressMiddlewareInterface, UnauthorizedError, BadRequestError} from "routing-controllers";
 
 export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
     if (req.isAuthenticated()) {
@@ -23,13 +24,53 @@ export class AuthMiddleware {
         private readonly userFinder: UserFinder
     ) {
     }
+
     public emailExists = async (req: Request, res: Response, next: NextFunction) => {
 
-        const { email } = req.body;
+        const {email} = req.body;
         const exists = await this.userFinder.executeByEmail(email);
 
         return isRight(exists)
             ? ResponseEntity.status(400).body(AuthError.DUPLICATED_EMAIL).send(res)
             : next();
+    }
+}
+
+@injectable()
+export class EmailExists implements ExpressMiddlewareInterface {
+    constructor(
+         private userFinder: UserFinder
+    ){}
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        const {email} = req.body;
+        const exists = await this.userFinder.executeByEmail(email);
+
+        return isRight(exists)
+            ? ResponseEntity.status(400).body(AuthError.DUPLICATED_EMAIL).send(res)
+            : next();
+    }
+}
+
+@injectable()
+export class IsAuthenticated implements ExpressMiddlewareInterface {
+    use(request: Request, response: Response, next: NextFunction) {
+
+        if (request.isAuthenticated()) return next();
+
+        return next(new UnauthorizedError(AuthError.NOT_AUTHENTICATED));
+    }
+}
+
+@injectable()
+export class Logout implements ExpressMiddlewareInterface {
+    use (req: Request, res: Response, next: NextFunction){
+        req.logout((err) => {
+            if (err) {
+                return new BadRequestError(err);
+            }
+        });
+
+        return next();
     }
 }
