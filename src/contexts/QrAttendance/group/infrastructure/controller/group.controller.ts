b317dae9e745251cf7aa60943bcf5d2408cc73e5
limberMay <limbermay1@gmail.com>
2023-01-30 23:41:43 -0600

    (src/contexts/qrAttendance/group) routing-controller implementado en el controlador de group con sus decoradores

--- src/contexts/QrAttendance/group/infrastructure/controller/group.controller.ts
+++ src/contexts/QrAttendance/group/infrastructure/controller/group.controller.ts
@@ -1,10 +1,13 @@
-import {GroupCreator, GroupDeleter, GroupFinder, GroupUpdater} from "../../application/useCases";
-import {Request, Response} from "express";
+import {Request} from "express";
+import {injectable} from "inversify";
 import {isRight} from "fp-ts/Either";
+import {GroupCreator, GroupDeleter, GroupFinder, GroupUpdater} from "../../application/useCases";
 import {ResponseEntity} from "../../../../shared/infrastructure/entities/response.entity";
 import {GroupError} from "../../application/errors/group.errors";
-import {injectable} from "inversify";
+import {BodyParam, Delete, Get, JsonController, Param, Post, Put, Req, UseBefore} from "routing-controllers";
+import {IsAuthenticated} from "../../../auth/infrastructure/middlewares";
 
+@JsonController('/group')
 @injectable()
 export class GroupController {
     constructor(
@@ -14,109 +17,126 @@ export class GroupController {
         private groupDeleter: GroupDeleter,
     ) {}
 
-    getGroup = async (req: Request, res: Response) => {
-        if (!req.user) return res.json({msg: 'NOT AUTHENTICATED'});
+    @Get('/:id')
+    @UseBefore(IsAuthenticated)
+    public async getGroup (@Param("id") id: string, @Req() req: Request) {
 
-        const { id: userId } = req.user;
-        const { id } = req.body;
+        const { id: userId } = req.authenticatedUser;
 
         const group = await this.groupFinder.execute(id, userId);
-        if (isRight(group)) return ResponseEntity.ok().body(group.right).send(res);
+        if (isRight(group))
+            return ResponseEntity
+                .ok()
+                .body(group.right)
+                .buid();
 
-        return this.handleError(group.left, res);
+        return this.handleError(group.left);
     }
 
-    getGroups = async(req: Request, res: Response) => {
-        if (!req.user) return res.json({msg: 'NOT AUTHENTICATED'});
-
-        const { id: userId } = req.user;
+    @Get('/all')
+    @UseBefore(IsAuthenticated)
+    public async getGroups (req: Request) {
+        const { id: userId } = req.authenticatedUser;
 
         const groups = await this.groupFinder.executeByUserId(userId);
-        if (isRight(groups)) return ResponseEntity.ok().body(groups.right).send(res);
+        if (isRight(groups))
+            return ResponseEntity
+                .ok()
+                .body(groups.right)
+                .buid();
 
-        return this.handleError(groups.left, res);
+        return this.handleError(groups.left);
     }
 
-    create = async (req: Request, res: Response) => {
-        if (!req.user) return res.json({msg: 'NOT AUTHENTICATED'});
+    @Post('/create')
+    public async create (@BodyParam("name") name: string, req: Request) {
 
-        const { id: userId } = req.user;
-        const { name } = req.body;
+        const { id: userId } = req.authenticatedUser;
 
         const group = await this.groupCreator.execute(name, userId);
 
-        if (isRight(group)) return ResponseEntity.ok().body(group.right).send(res);
+        if (isRight(group))
+            return ResponseEntity
+                .ok()
+                .body(group.right)
+                .buid();
 
-        return this.handleError(group.left, res);
+        return this.handleError(group.left);
     }
 
-    update = async(req: Request, res: Response) => {
-        if (!req.user) return res.status(401).send({message: "NOT_AUTHORIZED"});
+    @Put('/update')
+    @UseBefore(IsAuthenticated)
+    public async update (req: Request, @BodyParam("id") id: string, @BodyParam("updatedFields") updatedFields: any) {
 
-        const {id, updatedFields } = req.body;
-        const { id: userId } = req.user;
+        const { id: userId } = req.authenticatedUser;
 
         const expectedFields = {
             name: updatedFields.name
         }
 
         const result = await this.groupUpdater.execute(id, userId, expectedFields);
-        if (isRight(result)) return ResponseEntity.ok().body(result.right).send(res);
+        if (isRight(result))
+            return ResponseEntity
+                .ok()
+                .body(result.right)
+                .buid();
 
-        this.handleError(result.left, res);
+        this.handleError(result.left);
     }
 
-    public delete = async (req: Request, res: Response) => {
+    @Delete('/delete/:id')
+    @UseBefore(IsAuthenticated)
+    public async delete (@Param("id") id: string, req: Request) {
 
-        if (!req.user) return res.json({msg: 'NOT AUTHENTICATED'});
-
-        const { id: userId } = req.user;
-        const { id } = req.body;
-
+        const { id: userId } = req.authenticatedUser;
         const group = await this.groupDeleter.execute(id, userId);
 
-        if (isRight(group)) return ResponseEntity.ok().body(group.right).send(res);
+        if (isRight(group))
+            return ResponseEntity
+                .ok()
+                .body(group.right)
+                .buid();
 
-        this.handleError(group.left, res);
+        this.handleError(group.left);
     }
 
-    private handleError = (result: GroupError, res: Response) => {
+    private handleError = (result: GroupError) => {
         switch (result) {
             case GroupError.GROUP_NOT_FOUND:
                 return ResponseEntity
                     .status(404)
                     .body(result)
-                    .send(res);
+                    .buildError()
 
             case GroupError.GROUP_CANNOT_BE_FOUND:
                 return ResponseEntity
                     .status(500)
                     .body(result)
-                    .send(res);
+                    .buildError();
 
             case GroupError.GROUP_CANNOT_BE_CREATED:
                 return ResponseEntity
                     .status(500)
                     .body(result)
-                    .send(res);
+                    .buildError();
 
             case GroupError.GROUP_CANNOT_BE_UPDATED:
                 return ResponseEntity
                     .status(500)
                     .body(result)
-                    .send(res);
+                    .buildError();
 
             case GroupError.GROUP_CANNOT_BE_DELETED:
                 return ResponseEntity
                     .status(500)
                     .body(result)
-                    .send(res);
+                    .buildError();
 
             default:
                 return ResponseEntity
                     .status(500)
                     .body("INTERNAL_SERVER_ERROR")
-                    .send(res);
+                    .buildError();
         }
     }
 }
