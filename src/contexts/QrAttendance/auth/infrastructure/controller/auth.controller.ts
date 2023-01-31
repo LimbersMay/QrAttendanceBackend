3eddf73f1c680c54fd776866fd21ee94053da41f
limberMay <limbermay1@gmail.com>
2023-01-30 21:36:50 -0600

    (src/apps/qrAttendance) auth.controller.ts re definido usando routing-controller con decoradores

--- src/contexts/QrAttendance/auth/infrastructure/controller/auth.controller.ts
+++ src/contexts/QrAttendance/auth/infrastructure/controller/auth.controller.ts
@@ -1,100 +1,76 @@
 import {Request, Response} from "express";
 import {injectable} from "inversify";
-import {isRight} from "fp-ts/Either";
-import passport from "passport";
+import {Body, Controller, Get, Post, Req, Res, UseAfter, UseBefore} from "routing-controllers";
 import {UserCreator} from "../../../user/application/useCases";
 import {ResponseEntity} from "../../../../shared/infrastructure/entities/response.entity";
+import {Logout} from "../middlewares";
+import {Authenticate, InvalidCredentialsHandler} from "../middlewares/providers.middleware";
+import {isRight} from "fp-ts/Either";
 import {AuthError} from "../../application/errors/authError";
+import {UserError} from "../../../user/domain/errors/userError";
 
+@Controller('/auth')
 @injectable()
 export class AuthController {
     constructor(
         private readonly userCreator: UserCreator
-    ) {
-    }
+    ) {}
 
-    public login = (req: Request, res: Response) => {
-        passport.authenticate('local', (err, user) => {
-            if (err) {
-                return ResponseEntity
-                    .status(400)
-                    .body(err)
-                    .send(res);
-            }
-
-            if (!user) {
-                return ResponseEntity
-                    .status(400)
-                    .body(AuthError.INVALID_CREDENTIALS)
-                    .send(res);
-            }
-
-            req.logIn(user, (err) => {
-                if (err) {
-                    return ResponseEntity
-                        .status(400)
-                        .body(err)
-                        .send(res);
-                }
-
-                return res.status(200).json({
-                    ok: true,
-                    msg: "Login",
-                    user: user
-                });
-            });
-
-        })(req, res);
-    }
-
-    public register = async(req: Request, res: Response) => {
-
-        let { name, email,  password, lastname } = req.body;
-
-        const user = await this.userCreator.execute({ name, email, password, lastname });
-        console.log(user);
-
-        return isRight(user)
-            ? this.login(req, res)
-                : ResponseEntity
-                    .status(400)
-                    .body(user.left)
-                    .send(res);
-
-    }
-
-    public logout = (req: Request, res: Response) => {
-        req.logout((err) => {
-            if (err) {
-                return ResponseEntity
-                    .status(400)
-                    .body(err)
-                    .send(res);
-            }
-        });
+    @Post('/login')
+    @UseBefore(Authenticate)
+    @UseAfter(InvalidCredentialsHandler)
+    public login (@Req() req: Request) {
 
         return ResponseEntity
             .status(200)
-            .body("logout")
-            .send(res);
+            .body(req.user)
+            .buid()
     }
 
-    public isAuthenticated = (req: Request, res: Response) => {
-        const isAuthenticated = req.isAuthenticated();
+    @Post('/register')
+    public async register (@Body() { name, email, password, lastname}: { name: string, email: string, password: string, lastname: string}, @Res() res: Response) {
+        const result = await this.userCreator.execute({ name, email, password, lastname });
 
-        if (!isAuthenticated) {
-            return ResponseEntity
-                .status(200)
-                .body(AuthError.INVALID_CREDENTIALS)
-                .send(res);
+        if (isRight(result)) return res.redirect('/auth/login');
+
+        switch (result.left) {
+            case UserError.DUPLICATED_EMAIL:
+                return ResponseEntity
+                    .status(400)
+                    .body(result.left)
+                    .buid()
+
+            case UserError.USER_CANNOT_BE_CREATED:
+                return ResponseEntity
+                    .status(500)
+                    .body(result.left)
+                    .buid()
         }
 
-        const user = req.user;
+        return res.redirect('/auth/login');
+    }
 
-        return res.status(200).json({
-            ok: true,
-            msg: "Authenticated",
-            user
-        });
+    @Post('/logout')
+    @UseBefore(Logout)
+    public logout () {
+        return ResponseEntity
+            .status(200)
+            .body('Logout success')
+            .buid()
+    }
+
+    @Get('/authenticated')
+    public isAuthenticated( @Req() req: Request) {
+
+        if (!req.isAuthenticated()) return ResponseEntity
+            .status(200)
+            .body(AuthError.NOT_AUTHENTICATED)
+            .buid()
+
+
+        return ResponseEntity
+            .status(200)
+            .body(req.user)
+            .buid()
     }
 }
