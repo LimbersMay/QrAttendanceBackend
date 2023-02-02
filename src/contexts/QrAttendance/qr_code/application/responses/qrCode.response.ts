0bb691157655ccd690286dfed15f579a6386f3d2
limberMay <limbermay1@gmail.com>
2023-02-02 17:32:11 -0600

    (checkingForm) ownerId devuelto en la respuesta de QrCodeResponse

--- src/contexts/QrAttendance/qr_code/application/responses/qrCode.response.ts
+++ src/contexts/QrAttendance/qr_code/application/responses/qrCode.response.ts
@@ -3,15 +3,17 @@ import {QrCodeEntity} from "../../domain/qrCode.entity";
 export class QrCodeResponse {
     id: string;
     groupId: string;
+    ownerId: string;
     url: string;
     formId: string;
     name: string;
     enabled: boolean;
     manualRegistrationDate?: Date;
 
-    constructor(id: string, groupId: string, name: string, url: string, formId: string, enabled: boolean, manualRegistrationDate?: Date) {
+    constructor(id: string, groupId: string, ownerId: string, name: string, url: string, formId: string, enabled: boolean, manualRegistrationDate?: Date) {
         this.id = id;
         this.groupId = groupId;
+        this.ownerId = ownerId;
         this.name = name;
         this.url = url;
         this.formId = formId;
@@ -20,7 +22,7 @@ export class QrCodeResponse {
     }
 
     static fromQrCode(qrCode: QrCodeEntity): QrCodeResponse {
-        return new QrCodeResponse(qrCode.qrId, qrCode.groupId, qrCode.name, qrCode.url,qrCode.formId, qrCode.enabled, qrCode.manualRegistrationDate);
+        return new QrCodeResponse(qrCode.qrId, qrCode.groupId, qrCode.ownerId, qrCode.name, qrCode.url,qrCode.formId, qrCode.enabled, qrCode.manualRegistrationDate);
     }
 
     static fromQrCodes(qrCodes: QrCodeEntity[]): QrCodeResponse[] {
