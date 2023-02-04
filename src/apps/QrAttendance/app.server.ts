6536d0e0cfc9e9ffb572c65cbabe6fd15d5ce4de
limberMay <limbermay1@gmail.com>
2023-02-04 14:01:51 -0600

    (contexts/qrAttendance/auth) opción httpOnly agregado a las cookies del usaurio

--- src/apps/QrAttendance/app.server.ts
+++ src/apps/QrAttendance/app.server.ts
@@ -81,6 +81,7 @@ export class Server {
                 sameSite: 'none',
                 maxAge: 1000 * 60 * 60 * 24, // 1 day
                 secure: true,
+                httpOnly: true
             }
         }));
 
