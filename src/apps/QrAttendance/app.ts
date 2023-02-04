805f155c48863c562b13e5ea40102762bb3b3b5a
limberMay <limbermay1@gmail.com>
2023-02-03 21:22:10 -0600

    (qrAttendance) archivo .env de ejemplo de producción agregado

--- src/apps/QrAttendance/app.ts
+++ src/apps/QrAttendance/app.ts
@@ -1,4 +1,9 @@
-import 'dotenv/config';
+import {config} from 'dotenv';
+
+config({
+    path: __dirname + '/.production.example.env'
+})
+
 import {Server} from "./app.server";
 
 const server = new Server();
--- src/contexts/shared/infrastructure/db/mysql.connection.ts
+++ src/contexts/shared/infrastructure/db/mysql.connection.ts
@@ -4,8 +4,8 @@ import Group from "../../../QrAttendance/group/infrastructure/model/group.schema
 import QrCode from "../../../QrAttendance/qr_code/infrastructure/models/qrCode.schema";
 import Registry from "../../../QrAttendance/registry/infrastructure/model/registry.schema";
 
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-    host: process.env.MYSQL_HOST,
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE ?? '', process.env.MYSQL_USER ?? '', process.env.MYSQL_PASSWORD ?? '', {
+    host: process.env.MYSQL_HOST ?? '',
     dialect: 'mysql',
     logging: false
 });
