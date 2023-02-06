4233f4ec319012cdbd7a6c2057aa468522ba47bd
limberMay <limbermay1@gmail.com>
2023-02-06 00:18:21 -0600

    (contexts/qrAttendance) archivo secrets.ts creado para almacenar las variables de entorno y hacer comprobaciones

---
+++ src/contexts/utils/secrets.ts
@@ -0,0 +1,5 @@
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID';
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || 'YOUR_GOOGLE_CLIENT_SECRET';
+
+export const API_URL = process.env.API_URL || 'http://localhost:3000';
+export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
