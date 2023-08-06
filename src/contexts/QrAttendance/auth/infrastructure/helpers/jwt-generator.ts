ec8a73f58ca58b2e0b1430d7ca86928016325228
LimbersMay <limbermay1@gmail.com>
2023-08-06 11:18:53 -0600

    (src/qrAttendance/auth/helpers jwt-generator.ts) Funciones para la creación y verificación del token agregadas

---
+++ src/contexts/QrAttendance/auth/infrastructure/helpers/jwt-generator.ts
@@ -0,0 +1,32 @@
+import {sign, verify} from "jsonwebtoken";
+import {JWT_SECRET} from "../../../../utils/secrets";
+import {UserResponse} from "../../../user/application/responses/user.response";
+
+interface MyJwtPayload {
+    user: UserResponse;
+    iat: number;
+    exp: number;
+}
+
+export const generateToken = (user: UserResponse): string => {
+    return sign({ user }, JWT_SECRET, {
+        expiresIn: '2d'
+    });
+}
+
+export const verifyToken = (token: string): Promise<UserResponse> => {
+    return new Promise((resolve, reject) => {
+        verify(token, JWT_SECRET, (err, payload) => {
+            if (err) {
+                return reject(err);
+            }
+
+            if (!payload) {
+                return reject(new Error('No payload'));
+            }
+
+            const myPayload = payload as MyJwtPayload;
+            return resolve(myPayload.user);
+        });
+    });
+}
