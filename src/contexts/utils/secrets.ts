e56cdaa30023149ae331ea1d27f86037cc52fbab
LimbersMay <limbermay1@gmail.com>
2023-08-06 11:21:02 -0600

    (src/contexts/utils secrets.ts) Variable de entorno JWT_SECRET agregado

--- src/contexts/utils/secrets.ts
+++ src/contexts/utils/secrets.ts
@@ -1,3 +1,5 @@
+export const JWT_SECRET = process.env.JWT_SECRET || 'your jwt secret';
+
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID';
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || 'YOUR_GOOGLE_CLIENT_SECRET';
 
