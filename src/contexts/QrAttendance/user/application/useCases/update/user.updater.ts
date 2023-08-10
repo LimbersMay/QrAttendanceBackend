027e431f67ba2a059e046b3b25ac1fcc9c35d91a
LimbersMay <limbermay1@gmail.com>
2023-08-10 14:00:22 -0600

    (user.updater.ts) Método execute refactorizado, se usó try-catch en lugar de devolver la promesa directamente, y se recibió la criteria en lugar del ID concreto

--- src/contexts/QrAttendance/user/application/useCases/update/user.updater.ts
+++ src/contexts/QrAttendance/user/application/useCases/update/user.updater.ts
@@ -1,10 +1,9 @@
-import {UserRepository} from "../../../domain";
-import {UserQuery} from "../../../domain/user.query";
-import {Either} from "../../../../../shared/types/ErrorEither";
-import {UserError} from "../../../domain/errors/userError";
 import {isRight, left, right} from "fp-ts/Either";
 import {inject, injectable} from "inversify";
 import {TYPES} from "../../../../../../apps/QrAttendance/dependency-injection/user/types";
+import {Criteria} from "../../../../../shared/specifications/specification";
+import {Either} from "../../../../../shared/types/ErrorEither";
+import {UserQuery, UserError, UserRepository} from "../../../domain";
 
 @injectable()
 export class UserUpdater {
@@ -13,12 +12,14 @@ export class UserUpdater {
         @inject(TYPES.UserRepository) private userRepository: UserRepository
     ) {}
 
-   execute = (fields: UserQuery, userId: string): Promise<Either<UserError, number>> => {
-        return this.userRepository.updateUser(fields, userId).then((result) => {
-            return isRight(result)
-                ? right(result.right)
-                : left(UserError.USER_NOT_FOUND);
-
-        }).catch(() => left(UserError.USER_CANNOT_BE_UPDATED));
+   execute = async (fields: UserQuery, criteria: Criteria): Promise<Either<UserError, number>> => {
+       try {
+           const result = await this.userRepository.updateUser(fields, criteria);
+           return isRight(result)
+               ? right(result.right)
+               : left(UserError.USER_NOT_FOUND);
+       } catch {
+           return left(UserError.USER_CANNOT_BE_UPDATED);
+       }
     }
 }
