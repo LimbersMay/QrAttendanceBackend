d37f862fd98cf0055a0f32dcc502e7bc61da7980
LimbersMay <limbermay1@gmail.com>
2023-08-14 17:15:07 -0600

    (registry.updater.ts) Specifications recibidos al actualizar un registro y se resolvió la promesa con un try-catch

--- src/contexts/QrAttendance/registry/application/useCases/update/registry.updater.ts
+++ src/contexts/QrAttendance/registry/application/useCases/update/registry.updater.ts
@@ -1,9 +1,8 @@
-import * as E from 'fp-ts/lib/Either';
-import {RegistryErrors} from "../../../domain/registryErrors";
-import {RegistryRepository} from "../../../domain/registry.repository";
-import {RegistryQuery} from "../../../domain/registry.query";
+import { left, right, fold } from 'fp-ts/Either';
 import {inject, injectable} from "inversify";
 import {TYPES} from "../../../../../../apps/QrAttendance/dependency-injection/registry/types";
+import {Criteria, Either} from "../../../../shared";
+import {RegistryErrors, RegistryQuery, RegistryRepository} from "../../../domain";
 
 @injectable()
 export class RegistryUpdater {
@@ -11,14 +10,19 @@ export class RegistryUpdater {
         @inject(TYPES.RegistryRepository) private registryRepository: RegistryRepository
     ) {}
 
-    execute = async(fields: RegistryQuery, registryId: string, userId: string): Promise<E.Either<RegistryErrors, number>> => {
-        return this.registryRepository.updateRegistry(fields, registryId, userId).then(result => {
-            return E.fold(
-                () => E.left(RegistryErrors.REGISTRY_NOT_FOUND),
-                (rowsUpdated: number) => E.right(rowsUpdated)
-            )(result)
+    execute = async(fields: RegistryQuery, specifications: Criteria): Promise<Either<RegistryErrors, number>> => {
 
-        }).catch(() => E.left(RegistryErrors.REGISTRY_CANNOT_BE_UPDATED));
+        try {
+            const result = await this.registryRepository.updateRegistry(fields, specifications);
 
+            return fold(
+                () => left(RegistryErrors.REGISTRY_NOT_FOUND),
+                (rowsUpdated: number) => right(rowsUpdated)
+            )(result);
+
+        } catch (error) {
+            console.log(error);
+            return left(RegistryErrors.REGISTRY_CANNOT_BE_UPDATED);
+        }
     }
 }
