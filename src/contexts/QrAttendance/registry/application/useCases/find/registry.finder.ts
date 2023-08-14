5cbe82483865a40f00b9afb2c44629614886a028
LimbersMay <limbermay1@gmail.com>
2023-08-14 17:03:19 -0600

    (registry.finder.ts) Se optimizaron las importaciones y se usaron las specifications recibidas para buscar los registros deseados

--- src/contexts/QrAttendance/registry/application/useCases/find/registry.finder.ts
+++ src/contexts/QrAttendance/registry/application/useCases/find/registry.finder.ts
@@ -1,11 +1,9 @@
 import {inject, injectable} from "inversify";
-import * as E from 'fp-ts/lib/Either';
+import {fold, left, right} from 'fp-ts/Either';
 import {TYPES} from "../../../../../../apps/QrAttendance/dependency-injection/registry/types";
-import {Either} from "../../../../shared";
-import {RegistryResponse} from "../../responses/registry.response";
-import {RegistryRepository} from "../../../domain/registry.repository";
-import {RegistryErrors} from "../../../domain/registryErrors";
-import {RegistryEntity} from "../../../domain/registry.entity";
+import {Criteria, Either} from "../../../../shared";
+import {RegistryResponse} from "../../responses";
+import {RegistryEntity, RegistryErrors, RegistryRepository} from "../../../domain";
 
 @injectable()
 export class RegistryFinder {
@@ -14,23 +12,30 @@ export class RegistryFinder {
     ) {
     }
 
-    async execute(registryId: string, userId: string): Promise<Either<RegistryErrors, RegistryResponse>> {
-        return this.registryRepository.findRegistryById(registryId, userId).then(registry => {
-            return E.fold(
-                () => E.left(RegistryErrors.REGISTRY_NOT_FOUND),
-                (registry: RegistryEntity) => E.right(RegistryResponse.fromRegistry(registry))
-            )(registry)
+    async findOne(specifications: Criteria): Promise<Either<RegistryErrors, RegistryResponse>> {
 
-        }).catch(() => E.left(RegistryErrors.REGISTRY_CANNOT_BE_FOUND));
+        try {
+            const result = await this.registryRepository.findOne(specifications);
+
+            return fold(
+                () => left(RegistryErrors.REGISTRY_NOT_FOUND),
+                (registry: RegistryEntity) => right(RegistryResponse.fromRegistry(registry))
+            )(result)
+
+        } catch (error) {
+            console.log(error)
+            return left(RegistryErrors.REGISTRY_CANNOT_BE_FOUND);
+        }
     }
 
-    executeByUserId = async(userId: string): Promise<Either<RegistryErrors, RegistryResponse[]>> => {
-        return this.registryRepository.findRegistriesByUserId(userId).then(registries => {
-            return E.fold(
-                () => E.left(RegistryErrors.REGISTRIES_NOT_FOUND),
-                (registries: RegistryEntity[]) => E.right(RegistryResponse.fromRegistries(registries))
-            )(registries)
+    public async findAll(specifications: Criteria): Promise<Either<RegistryErrors, RegistryResponse[]>> {
 
-        }).catch(() => E.left(RegistryErrors.REGISTRIES_CANNOT_BE_FOUND));
+        try {
+            const registries = await this.registryRepository.findAll(specifications);
+            return right(RegistryResponse.fromRegistries(registries));
+        } catch (error) {
+            console.log(error)
+            return left(RegistryErrors.REGISTRIES_CANNOT_BE_FOUND);
+        }
     }
 }
