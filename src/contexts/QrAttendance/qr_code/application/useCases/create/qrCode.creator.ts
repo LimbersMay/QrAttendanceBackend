56cb0c0a594d232c51d19a8e0094bc2da0d1330f
LimbersMay <limbermay1@gmail.com>
2023-08-14 15:58:55 -0600

    (qrCode.creator.ts) Objeto CreateQrCodeDTO recibido al crear un nuevo registro de qr

--- src/contexts/QrAttendance/qr_code/application/useCases/create/qrCode.creator.ts
+++ src/contexts/QrAttendance/qr_code/application/useCases/create/qrCode.creator.ts
@@ -4,6 +4,7 @@ import {TYPES} from "../../../../../../apps/QrAttendance/dependency-injection/qr
 import {Either, UUIDGenerator} from "../../../../shared";
 import {QrCodeResponse} from "../../responses";
 import {QrCodeError, QrCodeRepository, QrCodeValue} from "../../../domain";
+import {CreateQrCodeDTO} from "../../validations/qrCode.create";
 
 
 @injectable()
@@ -13,17 +14,13 @@ export class QrCodeCreator {
         @inject(TYPES.QrCodeUUIDGenerator) private readonly uuidGenerator: UUIDGenerator
     ){}
 
-    public execute = async (name: string, groupId: string, userId: string, enabled: boolean, url: string, manualRegistrationDate: Date): Promise<Either<QrCodeError, QrCodeResponse>> => {
+    public async execute (qrCodeDTO: CreateQrCodeDTO, userId: string): Promise<Either<QrCodeError, QrCodeResponse>> {
 
         const qrCodeValue = QrCodeValue.create({
+            ...qrCodeDTO,
             qrId: this.uuidGenerator.random(),
-            groupId,
             ownerId: userId,
-            name,
-            url,
             formId: this.uuidGenerator.random(),
-            enabled,
-            manualRegistrationDate
         });
 
         try {
