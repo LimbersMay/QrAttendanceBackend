72c7bf97ef88b4741d0e33022f3a6509bb048dda
LimbersMay <limbermay1@gmail.com>
2023-08-15 09:50:55 -0600

    (registry.module.ts) importaciones del context registry optimizados

--- src/apps/QrAttendance/dependency-injection/registry/registry.module.ts
+++ src/apps/QrAttendance/dependency-injection/registry/registry.module.ts
@@ -1,17 +1,12 @@
 import {Container} from "inversify";
-import {RegistryCreator, RegistryFinder, RegistryUpdater, RegistryDeleter} from "../../../../contexts/QrAttendance/registry/application/useCases";
-import {RegistryRepository} from "../../../../contexts/QrAttendance/registry/domain/registry.repository";
 import {TYPES} from "./types";
-import {UuidAdapter} from "../../../../contexts/QrAttendance/registry/infrastructure/adapters";
 import {
-    RegistryMysqlRepository
-} from "../../../../contexts/QrAttendance/registry/infrastructure/repository/registry.repository";
-import {
-    RegistryController
-} from "../../../../contexts/QrAttendance/registry/infrastructure/controller/registry.controller";
-import {
-    RegistrySocketController
-} from "../../../../contexts/QrAttendance/registry/infrastructure/sockets/registrySocket.controller";
+    RegistryController,
+    RegistryCreator, RegistryDeleter, RegistryFinder, RegistryMysqlRepository, RegistryRepository,
+    RegistrySocketController, RegistryUpdater, UuidAdapter
+} from "../../../../contexts/QrAttendance/registry";
+
+import {UUIDGenerator} from "../../../../contexts/QrAttendance/shared";
 
 export const registryModule = (container: Container) => {
     container.bind<RegistryController>(RegistryController).toSelf();
@@ -19,11 +14,12 @@ export const registryModule = (container: Container) => {
     // websockets
     container.bind<RegistrySocketController>(RegistrySocketController).toSelf();
 
+    // use cases
     container.bind<RegistryCreator>(RegistryCreator).toSelf();
     container.bind<RegistryFinder>(RegistryFinder).toSelf();
     container.bind<RegistryUpdater>(RegistryUpdater).toSelf();
     container.bind<RegistryDeleter>(RegistryDeleter).toSelf();
 
     container.bind<RegistryRepository>(TYPES.RegistryRepository).to(RegistryMysqlRepository);
-    container.bind<UuidAdapter>(TYPES.RegistryUUIDGenerator).to(UuidAdapter);
+    container.bind<UUIDGenerator>(TYPES.RegistryUUIDGenerator).to(UuidAdapter);
 }
