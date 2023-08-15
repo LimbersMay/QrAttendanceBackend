20c098de6cbdbedda3d0fdbbd4e715727d529492
LimbersMay <limbermay1@gmail.com>
2023-08-14 19:11:06 -0600

    (registry.creator.ts) Al crear un registry, se recibe un objeto CreateRegistryDTO con todos los objetos validados

--- src/contexts/QrAttendance/registry/application/useCases/create/registry.creator.ts
+++ src/contexts/QrAttendance/registry/application/useCases/create/registry.creator.ts
@@ -1,11 +1,11 @@
 import {inject, injectable} from "inversify";
-import * as E from 'fp-ts/lib/Either';
+import {right, left } from 'fp-ts/Either';
 import {TYPES} from "../../../../../../apps/QrAttendance/dependency-injection/registry/types";
 import {Either, UUIDGenerator} from "../../../../shared";
-import {RegistryResponse} from "../../responses/registry.response";
-import {RegistryRepository} from "../../../domain/registry.repository";
-import {RegistryError} from "../../../domain/registryError";
-import {RegistryValue} from "../../../domain/registry.value";
+import {RegistryError, RegistryRepository, RegistryValue} from "../../../domain";
+import {RegistryResponse} from "../../responses";
+import {CreateRegistryDTO} from "../../validations/registry.create";
+
 
 @injectable()
 export class RegistryCreator {
@@ -14,24 +14,19 @@ export class RegistryCreator {
         @inject(TYPES.RegistryUUIDGenerator) private uuidGenerator: UUIDGenerator
     ){}
 
-    async execute(qrId: string, ownerId: string, name: string, group: string, career: string, firstSurname: string, secondSurname: string): Promise<Either<RegistryError, RegistryResponse>> {
+    async execute(registryDTO: CreateRegistryDTO, ownerId: string): Promise<Either<RegistryError, RegistryResponse>> {
 
         const registry = RegistryValue.create({
+            ...registryDTO,
             registryId: this.uuidGenerator.random(),
-            qrId,
             ownerId,
-            name,
-            group,
-            career,
-            firstSurname,
-            secondSurname
-        })
-
-        return this.registryRepository.createRegistry(registry).then(registry => {
-            return E.right(RegistryResponse.fromRegistry(registry))
-        }).catch((error) => {
-            console.log(error);
-            return E.left(RegistryError.REGISTRY_CANNOT_BE_CREATED);
         });
+
+        try {
+            const result = await this.registryRepository.createRegistry(registry);
+            return right(RegistryResponse.fromRegistry(result));
+        } catch (error) {
+            return left(RegistryError.REGISTRY_CANNOT_BE_CREATED);
+        }
     }
 }
